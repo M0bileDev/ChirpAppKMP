863e9e2f27b36e2a0907d5300bde7b9d23d07922
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-08 21:11:30 +0100

    feature(main): create modules relations
    Sanitized each module from test dependencies

--- feature/chat/presentation/build.gradle.kts
+++ feature/chat/presentation/build.gradle.kts
@@ -60,29 +60,18 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
-                // Add KMP dependencies here
+                implementation(projects.core.domain)
+                implementation(projects.core.designsystem)
+                implementation(projects.feature.chat.domain)
             }
         }
 
-        commonTest {
-            dependencies {
-                implementation(libs.kotlin.test)
-            }
-        }
 
         androidMain {
             dependencies {
                 // Add Android-specific dependencies here. Note that this source set depends on
                 // commonMain by default and will correctly pull the Android artifacts of any KMP
                 // dependencies declared in commonMain.
-            }
-        }
-
-        getByName("androidDeviceTest") {
-            dependencies {
-                implementation(libs.androidx.runner)
-                implementation(libs.androidx.test.core)
-                implementation(libs.androidx.junit)
             }
         }
 
