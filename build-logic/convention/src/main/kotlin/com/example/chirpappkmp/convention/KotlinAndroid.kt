b5779999e74fc33d2161ded53ab74b8193589800
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-09 22:14:24 +0100

    feature(convention): create kotlin  plugin function
    Configured complier options

--- build-logic/convention/src/main/kotlin/com/example/chirpappkmp/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/example/chirpappkmp/convention/KotlinAndroid.kt
@@ -4,6 +4,9 @@ import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 //Configure Kotlin language for the Android side
 internal fun Project.configureKotlinAndroid(
@@ -34,6 +37,18 @@ internal fun Project.configureKotlinAndroid(
             )
             //alternative
             //"coreLibraryDesugaring"(libs.findLibrary("android-desugarJdkLibs").get())
+        }
+    }
+}
+
+internal fun Project.configureKotlin() {
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+
+            freeCompilerArgs.add(
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi"
+            )
         }
     }
 }
