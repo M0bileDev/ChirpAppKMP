8e5e52fb93e798bdb7e442be896d71b5dde921b7
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-11 00:20:53 +0100

    feature(convention): create compose multiplatform plugin class
    Provided plugins. Configured targets. Set dependencies.

---
+++ build-logic/convention/src/main/kotlin/CmpApplicationConventionPlugin.kt
@@ -0,0 +1,29 @@
+import com.example.chirpappkmp.convention.configureAndroidTarget
+import com.example.chirpappkmp.convention.configureIosTargets
+import com.example.chirpappkmp.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class CmpApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.example.chirpappkmp.convention.android.application.compose")
+                //kmp
+                apply("org.jetbrains.kotlin.multiplatform")
+                //compose library references
+                apply("org.jetbrains.compose")
+                //compose compiler
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+
+            configureAndroidTarget()
+            configureIosTargets()
+
+            dependencies {
+                "debugImplementation"(libs.findLibrary("androidx-compose-ui-tooling").get())
+            }
+        }
+    }
+}
