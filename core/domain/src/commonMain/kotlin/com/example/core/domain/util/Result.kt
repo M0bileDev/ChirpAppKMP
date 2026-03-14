6c0b1b47bd525734be5356f3e1c9d8b381c57997
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-14 01:20:42 +0100

    feature(util): create utility function for empty result, create typealias for empty result

--- core/domain/src/commonMain/kotlin/com/example/core/domain/util/Result.kt
+++ core/domain/src/commonMain/kotlin/com/example/core/domain/util/Result.kt
@@ -34,3 +34,9 @@ inline fun <T, E : Error> Result<T, E>.onFailure(action: (E) -> Unit): Result<T,
         }
     }
 }
+
+fun <T, E : Error> Result<T, E>.asEmptyResult(): EmptyResult<E> {
+    return map { }
+}
+
+typealias EmptyResult<E> = Result<Unit, E>
