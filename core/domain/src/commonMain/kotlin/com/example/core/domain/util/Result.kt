b5d4a1f13a2cbfd3bbbf0b59995402de17feccd6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-14 01:11:07 +0100

    feature(util): create result utility function
    Returns data transformation from T to R

--- core/domain/src/commonMain/kotlin/com/example/core/domain/util/Result.kt
+++ core/domain/src/commonMain/kotlin/com/example/core/domain/util/Result.kt
@@ -5,3 +5,9 @@ sealed interface Result<out D, out E : Error> {
     data class Failure<out E : Error>(val error: E) : Result<Nothing, E>
 }
 
+inline fun <T, E: Error, R> Result<T,E>.map(map: (T) -> R): Result<R,E>{
+    return when(this){
+        is Result.Failure -> Result.Failure(error)
+        is Result.Success -> Result.Success(map(this.data))
+    }
+}
