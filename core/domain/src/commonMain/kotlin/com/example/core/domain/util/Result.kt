566487856818096bf88a3cf2469e05090fb71f95
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-14 01:08:51 +0100

    feature(util): create result class
    Defined Success and Error case

---
+++ core/domain/src/commonMain/kotlin/com/example/core/domain/util/Result.kt
@@ -0,0 +1,7 @@
+package com.example.core.domain.util
+
+sealed interface Result<out D, out E : Error> {
+    data class Success<out D>(val data: D) : Result<D, Nothing>
+    data class Failure<out E : Error>(val error: E) : Result<Nothing, E>
+}
+
