c49e0accad69b9e91322181752ad6e6b698dc3db
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-15 00:32:07 +0100

    feature(util): create composable function that returns String based on UiText

--- core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/UiText.kt
+++ core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/UiText.kt
@@ -1,6 +1,8 @@
 package com.example.core.presentation.util
 
+import androidx.compose.runtime.Composable
 import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
 
 sealed interface UiText {
     data class DynamicString(val value: String) : UiText
@@ -8,4 +10,15 @@ sealed interface UiText {
         val id: StringResource,
         val args: Array<Any> = arrayOf()
     ) : UiText
+
+    @Composable
+    fun asString(): String {
+        return when (this) {
+            is DynamicString -> value
+            is Resource -> stringResource(
+                resource = id,
+                *args
+            )
+        }
+    }
 }
