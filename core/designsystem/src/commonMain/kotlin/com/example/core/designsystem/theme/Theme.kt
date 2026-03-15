762df7ac0c5ef29f2de14ec710a1f0b2b3f62089
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-15 13:46:06 +0100

    feature(theme): create light color scheme

---
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/theme/Theme.kt
@@ -0,0 +1,35 @@
+package com.example.core.designsystem.theme
+
+import androidx.compose.material3.lightColorScheme
+
+val LightColorScheme = lightColorScheme(
+    primary = ChirpBrand500,
+    onPrimary = ChirpBrand1000,
+    primaryContainer = ChirpBrand100,
+    onPrimaryContainer = ChirpBrand900,
+
+    secondary = ChirpBase700,
+    onSecondary = ChirpBase0,
+    secondaryContainer = ChirpBase100,
+    onSecondaryContainer = ChirpBase900,
+
+    tertiary = ChirpBrand900,
+    onTertiary = ChirpBase0,
+    tertiaryContainer = ChirpBrand100,
+    onTertiaryContainer = ChirpBrand1000,
+
+    error = ChirpRed500,
+    onError = ChirpBase0,
+    errorContainer = ChirpRed200,
+    onErrorContainer = ChirpRed600,
+
+    background = ChirpBrand1000,
+    onBackground = ChirpBase0,
+    surface = ChirpBase0,
+    onSurface = ChirpBase1000,
+    surfaceVariant = ChirpBase100,
+    onSurfaceVariant = ChirpBase900,
+
+    outline = ChirpBase1000Alpha8,
+    outlineVariant = ChirpBase200,
+)
