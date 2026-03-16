f47def4c8e15d548081a869db057e957773d6bfb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-16 23:49:20 +0100

    feature(data): provide ktor library

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -14,6 +14,7 @@ kotlin {
             dependencies {
                 implementation(libs.kotlin.stdlib)
                 implementation(projects.core.domain)
+                implementation(libs.bundles.ktor.common)
             }
         }
 
