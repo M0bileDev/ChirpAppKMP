a963d6a99b692836ec7405ad11ec96d7ab2c624c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-17 00:03:00 +0100

    feature(network): create function that uses argument and creates http route

--- core/data/src/commonMain/kotlin/com/example/core/data/network/HttpClientExtension.kt
+++ core/data/src/commonMain/kotlin/com/example/core/data/network/HttpClientExtension.kt
@@ -1,2 +1,12 @@
 package com.example.core.data.network
 
+//full url or relative paths (api)
+fun constructRoute(route:String): String{
+    return when{
+        //full route was passed return route
+        route.contains(UrlConstants.BASE_URL_HTTPS) -> route
+        //relative path was used
+        route.startsWith("/") -> "${UrlConstants.BASE_URL_HTTPS}$route"
+        else -> "${UrlConstants.BASE_URL_HTTPS}/$route"
+    }
+}
