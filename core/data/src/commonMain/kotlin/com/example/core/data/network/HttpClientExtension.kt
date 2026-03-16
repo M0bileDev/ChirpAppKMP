1944f211b158b3e824b0379f7ff0ac8d3acc26ff
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-17 00:14:38 +0100

    feature(network): create function that transforms response to result

--- core/data/src/commonMain/kotlin/com/example/core/data/network/HttpClientExtension.kt
+++ core/data/src/commonMain/kotlin/com/example/core/data/network/HttpClientExtension.kt
@@ -1,12 +1,41 @@
 package com.example.core.data.network
 
+import com.example.core.domain.util.DataError
+import com.example.core.domain.util.Result
+import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.client.call.body
+import io.ktor.client.statement.HttpResponse
+
 //full url or relative paths (api)
-fun constructRoute(route:String): String{
-    return when{
+fun constructRoute(route: String): String {
+    return when {
         //full route was passed return route
         route.contains(UrlConstants.BASE_URL_HTTPS) -> route
         //relative path was used
         route.startsWith("/") -> "${UrlConstants.BASE_URL_HTTPS}$route"
         else -> "${UrlConstants.BASE_URL_HTTPS}/$route"
+    }
+}
+
+suspend inline fun <reified T> responseToResult(response: HttpResponse): Result<T, DataError.Remote> {
+    return when (response.status.value) {
+        in 200..299 -> {
+            try {
+                Result.Success(response.body<T>())
+            } catch (e: NoTransformationFoundException) {
+                Result.Failure(DataError.Remote.SERIALIZATION)
+            }
+        }
+        400 -> Result.Failure(DataError.Remote.BAD_REQUEST)
+        401 -> Result.Failure(DataError.Remote.UNAUTHORIZED)
+        403 -> Result.Failure(DataError.Remote.FORBIDDEN)
+        404 -> Result.Failure(DataError.Remote.NOT_FOUND)
+        408 -> Result.Failure(DataError.Remote.REQUEST_TIMEOUT)
+        409 -> Result.Failure(DataError.Remote.CONFLICT)
+        413 -> Result.Failure(DataError.Remote.PAYLOAD_TOO_LARGE)
+        429 -> Result.Failure(DataError.Remote.TOO_MANY_REQUESTS)
+        500 -> Result.Failure(DataError.Remote.SERVER_ERROR)
+        503 -> Result.Failure(DataError.Remote.SERVICE_UNAVAILABLE)
+        else -> Result.Failure(DataError.Remote.UNKNOWN)
     }
 }
