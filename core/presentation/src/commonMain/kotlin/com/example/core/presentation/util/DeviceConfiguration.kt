bd7cdcfadeb57bd390c8374ec9b4d75508c1c6dd
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-18 00:13:04 +0100

    feature(util): create enum class representation of device configurations

---
+++ core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/DeviceConfiguration.kt
@@ -0,0 +1,9 @@
+package com.example.core.presentation.util
+
+enum class DeviceConfiguration {
+    MOBILE_PORTRAIT,
+    MOBILE_LANDSCAPE,
+    TABLET_PORTRAIT,
+    TABLET_LANDSCAPE,
+    DESKTOP
+}
