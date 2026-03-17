7320a2cfb7fb7b919d458bffd65fe3b17d0fee4d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-17 23:14:29 +0100

    feature(network): create http client put method

--- core/data/src/commonMain/kotlin/com/example/core/data/network/HttpClientExtension.kt
+++ core/data/src/commonMain/kotlin/com/example/core/data/network/HttpClientExtension.kt
@@ -10,6 +10,7 @@ import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
@@ -33,6 +34,24 @@ suspend inline fun <reified Request, reified Response : Any> HttpClient.post(
 ): Result<Response, DataError.Remote> {
     return safeCall {
         post {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Request, reified Response : Any> HttpClient.put(
+    route: String,
+    queryParams: Map<String, String> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): Result<Response, DataError.Remote> {
+    return safeCall {
+        put {
             url(constructRoute(route))
             queryParams.forEach { (key, value) ->
                 parameter(key, value)
