6bd022053a5f91f8eac832e15716c0762ed723ba
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-20 22:14:21 +0100

    refactor(textfields): apply common text field layout component
    Extracted styled modifier

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpTextField.kt
@@ -1,17 +1,9 @@
 package com.example.core.designsystem.components.textfields
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -20,9 +12,6 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
@@ -45,79 +34,38 @@ fun ChirpTextField(
     keyboardType: KeyboardType = KeyboardType.Text,
     onFocusChanged: (Boolean) -> Unit = {},
 ) {
-
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-
-    val isFocused by interactionSource.collectIsFocusedAsState()
-
-    LaunchedEffect(isFocused) {
-        onFocusChanged(isFocused)
-    }
-
-    Column(
-        modifier = modifier
-    ) {
-        ChirpTextFieldTitle(title)
+    ChirpTextFieldLayout(
+        modifier = modifier,
+        title = title,
+        supportingText = supportingText,
+        isError = isError,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged,
+    ) { styleModifier, interactionSource ->
         ChirpTextFieldBody(
-            isFocused,
             enabled,
-            isError,
             state,
             singleLine,
             keyboardType,
             interactionSource,
-            placeholder
+            placeholder,
+            styleModifier
         )
-        ChirpTextFieldSupportingText(supportingText, isError)
-    }
-}
-
-@Composable
-private fun ChirpTextFieldTitle(title: String?) {
-    if (title != null) {
-        Text(
-            text = title,
-            style = MaterialTheme.typography.labelSmall,
-            color = MaterialTheme.colorScheme.extended.textSecondary
-        )
-        Spacer(modifier = Modifier.height(8.dp))
     }
 }
 
 @Composable
 private fun ChirpTextFieldBody(
-    isFocused: Boolean,
     enabled: Boolean,
-    isError: Boolean,
     state: TextFieldState,
     singleLine: Boolean,
     keyboardType: KeyboardType,
     interactionSource: MutableInteractionSource,
-    placeholder: String?
+    placeholder: String?,
+    modifier: Modifier = Modifier
 ) {
     BasicTextField(
-        modifier = Modifier.fillMaxWidth().background(
-            color = when {
-                isFocused -> MaterialTheme.colorScheme.primary.copy(
-                    alpha = 0.05f
-                )
-
-                enabled -> MaterialTheme.colorScheme.surface
-                else -> MaterialTheme.colorScheme.extended.secondaryFill
-            },
-            shape = RoundedCornerShape(8.dp)
-        ).border(
-            width = 1.dp,
-            color = when {
-                isError -> MaterialTheme.colorScheme.error
-                isFocused -> MaterialTheme.colorScheme.primary
-                else -> MaterialTheme.colorScheme.outline
-            },
-            shape = RoundedCornerShape(8.dp)
-        )
-            .padding(12.dp),
+        modifier = modifier,
         state = state,
         enabled = enabled,
         lineLimits = if (singleLine) {
@@ -153,22 +101,6 @@ private fun ChirpTextFieldBody(
             innerBox()
         }
     )
-}
-
-@Composable
-private fun ChirpTextFieldSupportingText(supportingText: String?, isError: Boolean) {
-    if (supportingText != null) {
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = supportingText,
-            color = if (isError) {
-                MaterialTheme.colorScheme.error
-            } else {
-                MaterialTheme.colorScheme.extended.textTertiary
-            },
-            style = MaterialTheme.typography.bodySmall
-        )
-    }
 }
 
 @Composable
