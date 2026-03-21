5606669a05732702cefce7e85044db8396f6b219
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-21 23:51:34 +0100

    feature(layouts): create component for authentication header

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/layouts/ChirpAdaptiveFormLayout.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/layouts/ChirpAdaptiveFormLayout.kt
@@ -1,15 +1,20 @@
 package com.example.core.designsystem.components.layouts
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.core.designsystem.theme.extended
 import com.example.core.presentation.composableUtil.currentDeviceConfiguration
@@ -46,9 +51,37 @@ fun ChirpAdaptiveFormLayout(
                 }
             )
         }
+
         DeviceConfiguration.MOBILE_LANDSCAPE -> TODO()
         DeviceConfiguration.TABLET_PORTRAIT,
         DeviceConfiguration.TABLET_LANDSCAPE,
-        DeviceConfiguration.DESKTOP -> {}
+        DeviceConfiguration.DESKTOP -> {
+        }
+    }
+}
+
+@Composable
+fun ColumnScope.AuthHeaderSection(
+    headerText: String,
+    headerColor: Color,
+    errorText: String? = null,
+) {
+    Text(
+        text = headerText,
+        style = MaterialTheme.typography.titleLarge,
+        color = headerColor,
+        textAlign = TextAlign.Center,
+        modifier = Modifier.fillMaxWidth()
+    )
+    AnimatedVisibility(
+        visible = errorText != null
+    ) {
+        if (errorText != null) {
+            Text(
+                text = errorText,
+                style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
     }
 }
