8790bed3eb0786bcc2cd4922cc2237567f62aaf2
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-21 23:49:26 +0100

    feature(layouts): add configuration, create header color based on configuration

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/layouts/ChirpAdaptiveFormLayout.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/layouts/ChirpAdaptiveFormLayout.kt
@@ -1,8 +1,12 @@
 package com.example.core.designsystem.components.layouts
 
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.core.designsystem.theme.extended
+import com.example.core.presentation.composableUtil.currentDeviceConfiguration
+import com.example.core.presentation.util.DeviceConfiguration
 
 @Composable
 fun ChirpAdaptiveFormLayout(
@@ -12,5 +16,10 @@ fun ChirpAdaptiveFormLayout(
     errorText: String? = null,
     formContent: @Composable ColumnScope.() -> Unit,
 ) {
-
+    val configuration = currentDeviceConfiguration()
+    val headerColor = if (configuration == DeviceConfiguration.MOBILE_LANDSCAPE) {
+        MaterialTheme.colorScheme.onBackground
+    } else {
+        MaterialTheme.colorScheme.extended.textPrimary
+    }
 }
