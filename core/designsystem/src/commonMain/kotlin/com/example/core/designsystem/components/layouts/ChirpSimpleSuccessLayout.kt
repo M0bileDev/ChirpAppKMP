1bad137500beda5b74ef8ce426062def6a79993d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-22 22:40:04 +0100

    feature(layouts): create simple success layout

---
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/layouts/ChirpSimpleSuccessLayout.kt
@@ -0,0 +1,11 @@
+package com.example.core.designsystem.components.layouts
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChirpSimpleSuccessLayout(
+    modifier: Modifier = Modifier
+) {
+
+}
