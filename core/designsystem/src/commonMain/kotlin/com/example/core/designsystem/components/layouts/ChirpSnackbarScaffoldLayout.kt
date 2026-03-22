3fe3a65794a09f2a964e228b84e9e0575cb03cb8
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-22 23:10:15 +0100

    feature(layouts): create snackbar scaffold layout

---
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/layouts/ChirpSnackbarScaffoldLayout.kt
@@ -0,0 +1,14 @@
+package com.example.core.designsystem.components.layouts
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChirpSnackbarScaffoldLayout(
+    snackbarHostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+
+}
