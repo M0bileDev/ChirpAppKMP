de08f454ad12f20173a9e69deab14b1c8ffaa8aa
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-23 23:43:57 +0100

    feature(register): create root screen that handles reference to view model and view state

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
@@ -0,0 +1,12 @@
+package com.example.feature.auth.presentation.register
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun RegisterRoot(
+    viewModel: RegisterViewModel = viewModel()
+) {
+
+}
+
