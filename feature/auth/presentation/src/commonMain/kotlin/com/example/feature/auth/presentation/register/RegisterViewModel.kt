754ab6c6d8cfdf5a8a089295bdb8524e44ec6b06
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-23 23:43:02 +0100

    feature(register): create function that handles screen actions

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterViewModel.kt
@@ -7,4 +7,10 @@ import kotlinx.coroutines.flow.asStateFlow
 class RegisterViewModel : ViewModel() {
     private val _state = MutableStateFlow(RegisterState())
     val state = _state.asStateFlow()
+
+    fun onAction(registerAction: RegisterAction){
+        when(registerAction){
+            else -> TODO("handle actions")
+        }
+    }
 }
