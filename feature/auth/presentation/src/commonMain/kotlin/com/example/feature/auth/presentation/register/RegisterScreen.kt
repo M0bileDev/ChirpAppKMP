57f6c98ce8903ad6d53e8bb2a6372f07cbe759d6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-23 23:44:22 +0100

    feature(register): create state used in screen view

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
@@ -1,12 +1,14 @@
 package com.example.feature.auth.presentation.register
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun RegisterRoot(
     viewModel: RegisterViewModel = viewModel()
 ) {
-
+    val state by viewModel.state.collectAsStateWithLifecycle()
 }
 
