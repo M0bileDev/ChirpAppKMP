4f8ba77be9cd44f47ccd4839471b0a3da7243bae
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-25 21:51:15 +0100

    feature(register): create group of actions that can be performed by user during register screen

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterAction.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterAction.kt
@@ -1,3 +1,8 @@
 package com.example.feature.auth.presentation.register
 
-sealed interface RegisterAction
+sealed interface RegisterAction{
+    data object OnInputTextFocusGain: RegisterAction
+    data object OnLoginClick: RegisterAction
+    data object OnRegisterClick: RegisterAction
+    data object OnTogglePasswordVisibilityClick: RegisterAction
+}
