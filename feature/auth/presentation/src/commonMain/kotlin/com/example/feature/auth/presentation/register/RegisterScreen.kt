10b3e511c6cb77dbaa666a4b6b4245a5471808b3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-27 00:40:02 +0100

    refactor(register): support ios platform;
    Until koin will be implemented view model has to be provided as factory;

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
@@ -35,7 +35,7 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun RegisterRoot(
-    viewModel: RegisterViewModel = viewModel()
+    viewModel: RegisterViewModel = viewModel { RegisterViewModel() }
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
     val snackbarHostState = remember { SnackbarHostState() }
