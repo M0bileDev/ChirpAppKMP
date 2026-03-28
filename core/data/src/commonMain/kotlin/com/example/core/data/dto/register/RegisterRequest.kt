3173033e33d8d9b76762322d2f4ce4e82511fe48
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-28 23:41:45 +0100

    feature(register): Create register dto model

---
+++ core/data/src/commonMain/kotlin/com/example/core/data/dto/register/RegisterRequest.kt
@@ -0,0 +1,10 @@
+package com.example.core.data.dto.register
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RegisterRequest(
+    val email: String,
+    val username: String,
+    val password: String
+)
