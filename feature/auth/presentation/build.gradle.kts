cd43109b9adeae3a9e2ea8657f8f0cd1c0c58d24
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-30 23:16:57 +0200

    feature(chirpappkmp): provide dependencies

--- feature/auth/presentation/build.gradle.kts
+++ feature/auth/presentation/build.gradle.kts
@@ -18,6 +18,7 @@ kotlin {
                 implementation(projects.core.designsystem)
                 implementation(compose.components.resources)
                 implementation(compose.components.uiToolingPreview)
+                implementation(libs.bundles.koin.common)
             }
         }
 
