6a5f1fef0c3040b273dcc4321ad78ba6e45c44d7
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-31 01:01:51 +0200

    feature(register_success): create actions for register success related screen

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register_success/RegisterSuccessAction.kt
@@ -0,0 +1,6 @@
+package com.example.feature.auth.presentation.register_success
+
+sealed interface RegisterSuccessAction {
+    data object OnLoginClick : RegisterSuccessAction
+    data object OnResendVerificationEmailClick : RegisterSuccessAction
+}
