74f405df36081b877c60716d0c421e1ff86cf9fc
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-30 23:17:44 +0200

    refactor(register): replace plain view model declaration with koin view model for compose

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register/RegisterScreen.kt
@@ -33,10 +33,11 @@ import com.example.core.designsystem.theme.ChirpTheme
 import com.example.core.presentation.util.ObserveAsEvents
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun RegisterRoot(
-    viewModel: RegisterViewModel = viewModel { RegisterViewModel() },
+    viewModel: RegisterViewModel = koinViewModel(),
     onRegisterSuccess: (String) -> Unit
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
