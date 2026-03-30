27e1bf8dace10238bc3b3669a970620282f7a0bc
m0bileDev <damian.ogorek.1996@gmail.com>
2026-03-30 23:09:25 +0200

    feature(di): create core data di module and provide dependencies;
    Created platform specific core data module

---
+++ core/data/src/commonMain/kotlin/com/example/core/data/di/CoreDataModule.kt
@@ -0,0 +1,24 @@
+package com.example.core.data.di
+
+import com.example.core.data.auth.KtorAuthService
+import com.example.core.data.logging.KermitLogger
+import com.example.core.data.network.HttpClientFactory
+import com.example.core.domain.auth.AuthService
+import com.example.core.domain.logging.ChirpLogger
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformCoreDataModule: Module
+
+val coreDataModule = module {
+
+    includes(platformCoreDataModule)
+
+    single<ChirpLogger> { KermitLogger }
+    single {
+        HttpClientFactory(get()).create(get())
+    }
+    singleOf(::KtorAuthService) bind AuthService::class
+}
