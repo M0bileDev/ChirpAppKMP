a5018338cb020beff88b56a6998595ef8d9e9228
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-01 00:07:57 +0200

    feature(navigation): create app root navigation
    Added auth graph

---
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/navigation/NavigationRoot.kt
@@ -0,0 +1,21 @@
+package com.example.chirpappkmp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.feature.auth.presentation.navigation.AuthGraphRoutes
+import com.example.feature.auth.presentation.navigation.authGraph
+
+@Composable
+fun NavigationRoot() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = AuthGraphRoutes.Graph
+    ) {
+        authGraph(
+            navController = navController,
+            onLoginSuccess = {}
+        )
+    }
+}
