31af6e8c06ffe4af9d587e89ab90874fdf0de81a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-01 00:09:00 +0200

    feature(chirpkmpapp): implement navigation on application level

--- composeApp/src/commonMain/kotlin/com/example/chirpappkmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/App.kt
@@ -1,18 +1,14 @@
 package com.example.chirpappkmp
 
 import androidx.compose.runtime.Composable
+import com.example.chirpappkmp.navigation.NavigationRoot
 import com.example.core.designsystem.theme.ChirpTheme
-import com.example.feature.auth.presentation.register.RegisterRoot
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     ChirpTheme {
-        RegisterRoot(
-            onRegisterSuccess = {
-
-            }
-        )
+        NavigationRoot()
     }
 }
