b44a8169d312f82d26397bf8745ffafbe05e6e26
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-02 20:39:32 +0200

    feature(email_verification): create view component that represents ongoing verification process

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/email_verification/EmailVerificationScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/email_verification/EmailVerificationScreen.kt
@@ -1,10 +1,26 @@
 package com.example.feature.auth.presentation.email_verification
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirpappkmp.feature.auth.presentation.generated.resources.Res
+import chirpappkmp.feature.auth.presentation.generated.resources.verifying_account
+import com.example.core.designsystem.components.layouts.ChirpAdaptiveResultLayout
+import com.example.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -26,4 +42,32 @@ fun EmailVerificationScreen(
     state: EmailVerificationState,
     snackbarHostState: SnackbarHostState,
     onAction: (EmailVerificationAction) -> Unit
-) = with(state) {}
+) = with(state) {
+    ChirpAdaptiveResultLayout {
+
+    }
+}
+
+@Composable
+fun VerifyingContent(modifier: Modifier) {
+    Column(
+        modifier = Modifier
+            .heightIn(min = 200.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(
+            16.dp,
+            Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.size(64.dp),
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = stringResource(Res.string.verifying_account),
+            color = MaterialTheme.colorScheme.extended.textSecondary,
+            style = MaterialTheme.typography.bodySmall
+        )
+    }
+}
