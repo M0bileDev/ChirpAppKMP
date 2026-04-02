4e3787747d6688dfe52582c0f3b8d27472149638
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-02 20:38:00 +0200

    refactor(email_verification): updated properties that represent state of email verification

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/email_verification/EmailVerificationState.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/email_verification/EmailVerificationState.kt
@@ -1,5 +1,6 @@
 package com.example.feature.auth.presentation.email_verification
 
 data class EmailVerificationState(
-    val arg0: Unit = Unit
+    val isVerifying: Boolean = false,
+    val isVerified: Boolean = false
 )
