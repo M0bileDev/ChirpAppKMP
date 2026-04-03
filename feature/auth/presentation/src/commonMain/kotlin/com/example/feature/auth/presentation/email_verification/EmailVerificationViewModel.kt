51ef43a2544bc222bd502a6fe543e123a3d44ae5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-03 23:23:47 +0200

    feature(email_verification): create logic that calls verification email; provide auth service and saved state handle (for navigation arguments)

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/email_verification/EmailVerificationViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/email_verification/EmailVerificationViewModel.kt
@@ -1,20 +1,30 @@
 package com.example.feature.auth.presentation.email_verification
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.domain.auth.AuthService
+import com.example.core.domain.util.onFailure
+import com.example.core.domain.util.onSuccess
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class EmailVerificationViewModel : ViewModel() {
+class EmailVerificationViewModel(
+    private val authService: AuthService,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
+    private val token = savedStateHandle.get<String>("token")
     private var hasLoadedInitialData = false
     private val _state = MutableStateFlow(EmailVerificationState())
     val state = _state
         .onStart {
             if (!hasLoadedInitialData) {
-                //todo
+                verifyEmail()
                 hasLoadedInitialData = true
             }
         }.stateIn(
@@ -23,5 +33,32 @@ class EmailVerificationViewModel : ViewModel() {
             initialValue = EmailVerificationState()
         )
 
-    fun onAction(emailVerificationAction: EmailVerificationAction) {}
+    private fun verifyEmail() = with(viewModelScope) {
+        _state.update {
+            it.copy(
+                isVerifying = true
+            )
+        }
+
+        launch {
+            authService
+                .verifyEmail(token ?: "Invalid token")
+                .onSuccess {
+                    _state.update {
+                        it.copy(
+                            isVerifying = false,
+                            isVerified = true
+                        )
+                    }
+                }
+                .onFailure {
+                    _state.update {
+                        it.copy(
+                            isVerifying = false,
+                            isVerified = false
+                        )
+                    }
+                }
+        }
+    }
 }
