708a79e812fb93b56b5e40329d27472e82dc41fa
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-03 23:22:14 +0200

    feature(di): register view model in di auth presentation module

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/di/AuthPresentationModule.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/di/AuthPresentationModule.kt
@@ -1,5 +1,6 @@
 package com.example.feature.auth.presentation.di
 
+import com.example.feature.auth.presentation.email_verification.EmailVerificationViewModel
 import com.example.feature.auth.presentation.register.RegisterViewModel
 import com.example.feature.auth.presentation.register_success.RegisterSuccessViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val authPresentationModule = module {
     viewModelOf(::RegisterViewModel)
     viewModelOf(::RegisterSuccessViewModel)
+    viewModelOf(::EmailVerificationViewModel)
 }
