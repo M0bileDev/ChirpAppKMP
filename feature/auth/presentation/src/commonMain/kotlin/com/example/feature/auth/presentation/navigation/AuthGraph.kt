731a28aa110f6e7da0c87a9aa13fc8aff301a151
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-04 00:45:48 +0200

    feature(navigation): setup deep link in navigation

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/navigation/AuthGraph.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/navigation/AuthGraph.kt
@@ -3,7 +3,9 @@ package com.example.feature.auth.presentation.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import androidx.navigation.navigation
+import com.example.feature.auth.presentation.email_verification.EmailVerificationRoot
 import com.example.feature.auth.presentation.register.RegisterRoot
 import com.example.feature.auth.presentation.register_success.RegisterSuccessRoot
 
@@ -23,6 +25,20 @@ fun NavGraphBuilder.authGraph(
         }
         composable<AuthGraphRoutes.RegisterSuccess> {
             RegisterSuccessRoot()
+        }
+        // token key used in the uri has to be the same as the parameter in data class EmailVerification
+        // same key will be used in view model as navigation result
+        composable<AuthGraphRoutes.EmailVerification>(
+            deepLinks = listOf(
+                navDeepLink {
+                    this.uriPattern = "https://chirp.pl-coding.com/api/auth/verify?token={token}"
+                },
+                navDeepLink {
+                    this.uriPattern = "chirp://chirp.pl-coding.com/api/auth/verify?token={token}"
+                },
+            )
+        ) {
+            EmailVerificationRoot()
         }
     }
 }
