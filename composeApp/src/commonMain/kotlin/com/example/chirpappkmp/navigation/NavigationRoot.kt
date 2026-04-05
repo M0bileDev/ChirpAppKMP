466341c52e50a2a0a7845daf1df981bb2de958d5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-05 23:54:34 +0200

    feature(navigation): create navigation logic to navigate from auth feature to chat list feature

--- composeApp/src/commonMain/kotlin/com/example/chirpappkmp/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/navigation/NavigationRoot.kt
@@ -3,8 +3,11 @@ package com.example.chirpappkmp.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.feature.auth.presentation.navigation.AuthGraphRoutes
 import com.example.feature.auth.presentation.navigation.authGraph
+import com.example.feature.chat.presentation.chat_list.ChatListRoute
+import com.example.feature.chat.presentation.chat_list.ChatListScreenRoot
 
 @Composable
 fun NavigationRoot(
@@ -16,7 +19,16 @@ fun NavigationRoot(
     ) {
         authGraph(
             navController = navController,
-            onLoginSuccess = {}
+            onLoginSuccess = {
+                navController.navigate(ChatListRoute) {
+                    popUpTo(AuthGraphRoutes.Graph) {
+                        inclusive = true
+                    }
+                }
+            }
         )
+        composable<ChatListRoute> {
+            ChatListScreenRoot()
+        }
     }
 }
