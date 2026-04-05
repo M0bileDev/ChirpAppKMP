b0cf59b4116587b8a952168a509020bc03246f59
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-05 02:56:15 +0200

    feature(login): setup view

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/login/LoginScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/login/LoginScreen.kt
@@ -1,24 +1,129 @@
 package com.example.feature.auth.presentation.login
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirpappkmp.feature.auth.presentation.generated.resources.Res
+import chirpappkmp.feature.auth.presentation.generated.resources.create_account
+import chirpappkmp.feature.auth.presentation.generated.resources.email
+import chirpappkmp.feature.auth.presentation.generated.resources.email_placeholder
+import chirpappkmp.feature.auth.presentation.generated.resources.forgot_password
+import chirpappkmp.feature.auth.presentation.generated.resources.login
+import chirpappkmp.feature.auth.presentation.generated.resources.password
+import chirpappkmp.feature.auth.presentation.generated.resources.welcome_back
+import com.example.core.designsystem.components.brand.ChirpBrandLogo
+import com.example.core.designsystem.components.buttons.ChirpButton
+import com.example.core.designsystem.components.buttons.ChirpButtonStyle
+import com.example.core.designsystem.components.layouts.ChirpAdaptiveFormLayout
+import com.example.core.designsystem.components.layouts.ChirpSnackbarScaffoldLayout
+import com.example.core.designsystem.components.textfields.ChirpPasswordTextField
+import com.example.core.designsystem.components.textfields.ChirpTextField
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
-fun LoadingScreenRoot(
-    viewModel: LoginViewModel = koinViewModel()
+fun LoginRoot(
+    viewModel: LoginViewModel = koinViewModel(),
+    onLoginSuccess: () -> Unit,
+    onForgotPasswordClick: () -> Unit,
+    onCreateAccountClick: () -> Unit
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val snackbarHostState = remember { SnackbarHostState() }
 
     LoginScreen(
         state = state,
-        onAction = viewModel::onAction
+        snackbarHostState = snackbarHostState,
+        onAction = { action ->
+            when (action) {
+                LoginAction.OnSignUpClick -> onCreateAccountClick()
+                LoginAction.OnForgotPasswordClick -> onForgotPasswordClick()
+                else -> Unit
+            }
+            viewModel.onAction(action)
+        }
     )
 }
 
 @Composable
 fun LoginScreen(
     state: LoginState,
+    snackbarHostState: SnackbarHostState,
     onAction: (LoginAction) -> Unit
-) = with(state) {}
+) = with(state) {
+    ChirpSnackbarScaffoldLayout(
+        snackbarHostState = snackbarHostState
+    ) {
+        ChirpAdaptiveFormLayout(
+            headerText = stringResource(Res.string.welcome_back),
+            errorText = error?.asString(),
+            logo = {
+                ChirpBrandLogo()
+            },
+            formContent = {
+                ChirpTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    state = emailTextFieldState,
+                    title = stringResource(Res.string.email),
+                    placeholder = stringResource(Res.string.email_placeholder),
+                    keyboardType = KeyboardType.Email,
+                    singleLine = true,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                ChirpPasswordTextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    state = emailTextFieldState,
+                    title = stringResource(Res.string.password),
+                    placeholder = stringResource(Res.string.password),
+                    onToggleVisibilityClick = {
+                        onAction(LoginAction.OnTogglePasswordVisibilityClick)
+                    },
+                    isPasswordVisible = isPasswordVisible
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .clickable {
+                            onAction(LoginAction.OnForgotPasswordClick)
+                        },
+                    text = stringResource(Res.string.forgot_password),
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                ChirpButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(Res.string.login),
+                    enabled = canLogin,
+                    isLoading = isLoggingIn,
+                    onClick = {
+                        onAction(LoginAction.OnLoginClick)
+                    }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                ChirpButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = stringResource(Res.string.create_account),
+                    style = ChirpButtonStyle.SECONDARY,
+                    onClick = {
+                        onAction(LoginAction.OnSignUpClick)
+                    }
+                )
+            }
+        )
+    }
+}
+
