8a4ce42ff1be7670741ddea80d0ff057696c79ef
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-06 20:36:41 +0200

    feature(register_success): add arguments that reference to log in navigation
    Update action logic

--- feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register_success/RegisterSuccessScreen.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/example/feature/auth/presentation/register_success/RegisterSuccessScreen.kt
@@ -31,7 +31,8 @@ import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun RegisterSuccessRoot(
-    viewModel: RegisterSuccessViewModel = koinViewModel()
+    viewModel: RegisterSuccessViewModel = koinViewModel(),
+    onLoginClick: () -> Unit
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
     val snackbarHostState = remember { SnackbarHostState() }
@@ -49,7 +50,13 @@ fun RegisterSuccessRoot(
     RegisterSuccessScreen(
         state = state,
         snackbarHostState = snackbarHostState,
-        onAction = viewModel::onAction
+        onAction = { action ->
+            when (action) {
+                RegisterSuccessAction.OnLoginClick -> onLoginClick()
+                else -> Unit
+            }
+            viewModel.onAction(action)
+        }
     )
 }
 
