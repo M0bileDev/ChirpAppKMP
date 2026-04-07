85ffc7d6bd5cee8c0713de0eaaf1ff0de293fc7d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-08 00:24:33 +0200

    feature(auth): create ios specific factory for data store preferences

---
+++ core/data/src/iosMain/kotlin/com/example/core/data/auth/createDataStore.ios.kt
@@ -0,0 +1,24 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
+package com.example.core.data.auth
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
+
+fun createDataStore(): DataStore<Preferences> {
+    val directory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null
+    )
+    requireNotNull(directory)
+    return createDataStore {
+        directory.path + "/$DATA_STORE_FILE_NAME"
+    }
+}
