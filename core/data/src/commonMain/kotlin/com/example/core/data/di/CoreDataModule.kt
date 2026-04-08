9b8f0ed9b328f3f9e114bf8bb900882e2b2a69a9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-08 23:55:51 +0200

    fix(di): provide dependency for SessionStorage

--- core/data/src/commonMain/kotlin/com/example/core/data/di/CoreDataModule.kt
+++ core/data/src/commonMain/kotlin/com/example/core/data/di/CoreDataModule.kt
@@ -20,7 +20,7 @@ val coreDataModule = module {
 
     single<ChirpLogger> { KermitLogger }
     single {
-        HttpClientFactory(get()).create(get())
+        HttpClientFactory(get(), get()).create(get())
     }
     singleOf(::KtorAuthService) bind AuthService::class
     singleOf(::DataStoreStorageSession) bind SessionStorage::class
