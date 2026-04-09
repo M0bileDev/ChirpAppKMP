31792348015f563d77e6e690154690e09bbd8d1d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-10 00:18:16 +0200

    refactor(navigation): add argument that represent conditional start destination

--- composeApp/src/commonMain/kotlin/com/example/chirpappkmp/navigation/NavigationRoot.kt
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/navigation/NavigationRoot.kt
@@ -11,11 +11,12 @@ import com.example.feature.chat.presentation.chat_list.ChatListScreenRoot
 
 @Composable
 fun NavigationRoot(
-    navController: NavHostController
+    navController: NavHostController,
+    startDestination: Any
 ) {
     NavHost(
         navController = navController,
-        startDestination = AuthGraphRoutes.Graph
+        startDestination = startDestination
     ) {
         authGraph(
             navController = navController,
