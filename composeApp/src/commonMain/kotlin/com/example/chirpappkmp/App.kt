ce324a2a94859effe9df97818c0e34c893587014
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-10 00:19:10 +0200

    feature(chirpappkmp): update root view that stores shared view model logic

--- composeApp/src/commonMain/kotlin/com/example/chirpappkmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/App.kt
@@ -1,19 +1,44 @@
 package com.example.chirpappkmp
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.example.chirpappkmp.navigation.DeepLinkListener
 import com.example.chirpappkmp.navigation.NavigationRoot
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.feature.auth.presentation.navigation.AuthGraphRoutes
+import com.example.feature.chat.presentation.chat_list.ChatListRoute
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App() {
+fun App(
+    onAuthenticationChecked: () -> Unit = {},
+    viewModel: MainViewModel = koinViewModel()
+) {
     val navController = rememberNavController()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(state.isCheckingAuth) {
+        if (!state.isCheckingAuth) {
+            onAuthenticationChecked()
+        }
+    }
 
     DeepLinkListener(navController)
     ChirpTheme {
-        NavigationRoot(navController)
+        if (!state.isCheckingAuth) {
+            NavigationRoot(
+                navController = navController,
+                startDestination = if (state.isLoggedIn) {
+                    ChatListRoute
+                } else {
+                    AuthGraphRoutes.Graph
+                }
+            )
+        }
     }
 }
