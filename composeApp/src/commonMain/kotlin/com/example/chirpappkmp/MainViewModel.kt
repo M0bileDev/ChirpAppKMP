4f6c46cb240bf6dd5c060e41212ce59c92a59cc3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-10 23:17:08 +0200

    feature(chirpappkmp): create logic that observes session and makes action when it is expired

--- composeApp/src/commonMain/kotlin/com/example/chirpappkmp/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/MainViewModel.kt
@@ -6,6 +6,8 @@ import com.example.core.domain.auth.SessionStorage
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -13,6 +15,7 @@ class MainViewModel(
     private val sessionStorage: SessionStorage
 ) : ViewModel() {
 
+    private var previousRefreshToken: String? = null
     private val _state = MutableStateFlow(MainState())
     val state = _state.asStateFlow()
 
@@ -26,5 +29,26 @@ class MainViewModel(
                 )
             }
         }
+    }
+
+    private fun observeSession(){
+        sessionStorage
+            .observeAuthInfo()
+            .onEach { authInfo ->
+                val currentRefreshToken = authInfo?.refreshToken
+                //user has been logged in, current refresh token is not null, during application
+                //lifecycle user made action that made token expired ->  currentRefreshToken = null
+                val isSessionExpired = previousRefreshToken != null && currentRefreshToken == null
+
+                if(isSessionExpired){
+                    sessionStorage.set(null)
+                    _state.update { it.copy(
+                        isLoggedIn = false
+                    ) }
+                }
+
+                previousRefreshToken = currentRefreshToken
+            }
+            .launchIn(viewModelScope)
     }
 }
