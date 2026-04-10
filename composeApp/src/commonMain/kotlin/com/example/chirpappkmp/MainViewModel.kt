a32be362b64785387da00dd68d3e005603279686
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-10 23:27:37 +0200

    feature(chirpappkmp): create logic that emits SessionExpired when session is expired
    Events will be observed inside App root view and navigation will be performed to log in screen if session is expired

--- composeApp/src/commonMain/kotlin/com/example/chirpappkmp/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/chirpappkmp/MainViewModel.kt
@@ -3,11 +3,15 @@ package com.example.chirpappkmp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.core.domain.auth.SessionStorage
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -16,8 +20,22 @@ class MainViewModel(
 ) : ViewModel() {
 
     private var previousRefreshToken: String? = null
+    private var hasLoadedInitialData = false
     private val _state = MutableStateFlow(MainState())
-    val state = _state.asStateFlow()
+    val state = _state
+        .onStart {
+            if (!hasLoadedInitialData) {
+                observeSession()
+                hasLoadedInitialData = true
+            }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000L),
+            initialValue = MainState()
+        )
+    private val eventChannel = Channel<MainEvent>()
+    val events = eventChannel.receiveAsFlow()
 
     init {
         viewModelScope.launch {
@@ -31,7 +49,7 @@ class MainViewModel(
         }
     }
 
-    private fun observeSession(){
+    private fun observeSession() {
         sessionStorage
             .observeAuthInfo()
             .onEach { authInfo ->
@@ -40,11 +58,14 @@ class MainViewModel(
                 //lifecycle user made action that made token expired ->  currentRefreshToken = null
                 val isSessionExpired = previousRefreshToken != null && currentRefreshToken == null
 
-                if(isSessionExpired){
+                if (isSessionExpired) {
                     sessionStorage.set(null)
-                    _state.update { it.copy(
-                        isLoggedIn = false
-                    ) }
+                    _state.update {
+                        it.copy(
+                            isLoggedIn = false
+                        )
+                    }
+                    eventChannel.send(MainEvent.OnSessionExpired)
                 }
 
                 previousRefreshToken = currentRefreshToken
