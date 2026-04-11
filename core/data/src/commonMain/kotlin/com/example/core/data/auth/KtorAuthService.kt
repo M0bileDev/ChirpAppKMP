cb78de113a76966b7efcfc1bcd99eb64d6a399d0
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-11 23:49:58 +0200

    feature(auth): create implementation to reset user password

--- core/data/src/commonMain/kotlin/com/example/core/data/auth/KtorAuthService.kt
+++ core/data/src/commonMain/kotlin/com/example/core/data/auth/KtorAuthService.kt
@@ -55,4 +55,11 @@ class KtorAuthService(
             authInfoSerializable.toDomain()
         }
     }
+
+    override suspend fun forgotPassword(email: String): EmptyResult<DataError.Remote> {
+        return httpClient.post<EmailRequest, Unit>(
+            route = "/auth/forgot-password",
+            body = EmailRequest(email)
+        )
+    }
 }
