6bf0455bba812761cb0c88192f7acc902e9040c6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-13 22:33:17 +0200

    feature(auth): define abstraction function for reset password

--- core/domain/src/commonMain/kotlin/com/example/core/domain/auth/AuthService.kt
+++ core/domain/src/commonMain/kotlin/com/example/core/domain/auth/AuthService.kt
@@ -25,4 +25,9 @@ interface AuthService {
     ): Result<AuthInfo, DataError.Remote>
 
     suspend fun forgotPassword(email: String): EmptyResult<DataError.Remote>
+
+    suspend fun resetPassword(
+        newPassword: String,
+        token: String
+    ): EmptyResult<DataError.Remote>
 }
