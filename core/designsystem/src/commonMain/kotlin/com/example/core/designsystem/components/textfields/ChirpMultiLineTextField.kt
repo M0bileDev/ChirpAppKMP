f83aef69182712b703c2828d18885fc0716a8dce
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-16 00:29:22 +0200

    feature(textfields): create preview for dark theme

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
@@ -110,3 +110,28 @@ fun PreviewChirpMultiLineTextField() {
         )
     }
 }
+
+@Preview
+@Composable
+fun PreviewDarkChirpMultiLineTextField() {
+    ChirpTheme(
+        darkTheme = true
+    ) {
+        ChirpMultiLineTextField(
+            modifier = Modifier
+                .widthIn(max = 300.dp)
+                .heightIn(max = 150.dp),
+            state = rememberTextFieldState(
+                initialText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            ),
+            placeholder = null,
+            bottomContent = {
+                Spacer(modifier = Modifier.weight(1f))
+                ChirpButton(
+                    text = "Lorem ipsum",
+                    onClick = {}
+                )
+            }
+        )
+    }
+}
