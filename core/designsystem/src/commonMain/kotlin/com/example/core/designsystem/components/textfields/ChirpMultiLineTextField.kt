dc74d3f01ca4852633ae13c32168322192341427
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-15 23:37:13 +0200

    feature(textfields): Add receiver type of RowScope to bottom content
    Created preview

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
@@ -1,10 +1,18 @@
 package com.example.core.designsystem.components.textfields
 
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.components.buttons.ChirpButton
+import com.example.core.designsystem.theme.ChirpTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ChirpMultiLineTextField(
@@ -14,7 +22,28 @@ fun ChirpMultiLineTextField(
     enabled: Boolean = true,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
-    bottomContent: @Composable (() -> Unit)? = null
+    bottomContent: @Composable (RowScope.() -> Unit)? = null
 ) {
 
+}
+
+@Preview
+@Composable
+fun PreviewChirpMultiLineTextField() {
+    ChirpTheme {
+        ChirpMultiLineTextField(
+            modifier = Modifier.widthIn(max = 300.dp),
+            state = rememberTextFieldState(
+                initialText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            ),
+            placeholder = null,
+            bottomContent = {
+                Spacer(modifier = Modifier.weight(1f))
+                ChirpButton(
+                    text = "Lorem ipsum",
+                    onClick = {}
+                )
+            }
+        )
+    }
 }
