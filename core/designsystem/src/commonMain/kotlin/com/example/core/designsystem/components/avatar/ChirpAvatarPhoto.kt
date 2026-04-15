7fd13d87fa2a5066d5ae443f79a33a661c960331
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-16 00:32:05 +0200

    feature(avatar): create type that represents photo size

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/avatar/ChirpAvatarPhoto.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/avatar/ChirpAvatarPhoto.kt
@@ -2,8 +2,15 @@ package com.example.core.designsystem.components.avatar
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.core.designsystem.theme.ChirpTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
+enum class AvatarSize(val dp: Dp){
+    REGULAR(40.dp),
+    LARGE(60.dp)
+}
 
 @Composable
 fun ChirpAvatarPhoto(modifier: Modifier = Modifier){
