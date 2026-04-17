c280985492703f106c97885f347eefbd2c734890
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-17 23:50:15 +0200

    refactor(dialogs): remove previews

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpAdaptiveDialogSheetLayout.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpAdaptiveDialogSheetLayout.kt
@@ -2,24 +2,11 @@ package com.example.core.designsystem.components.dialogs
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.core.designsystem.theme.ChirpTheme
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun ChirpAdaptiveDialogSheetLayout(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
-) {}
-
-@Preview
-@Composable
-fun PreviewChirpAdaptiveDialogSheetLayout() {
-    ChirpTheme { }
-}
-
-@Preview
-@Composable
-fun PreviewDarkChirpAdaptiveDialogSheetLayout() {
-    ChirpTheme(darkTheme = true) { }
+) {
 }
