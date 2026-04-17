9679670859d4da92e6fb83fe7a0a7c1419ae3631
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-17 23:23:37 +0200

    feature(dialogs): add content

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpBottomSheet.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpBottomSheet.kt
@@ -1,5 +1,11 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.core.designsystem.components.dialogs
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -10,6 +16,15 @@ fun ChirpBottomSheet(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
+
+    ModalBottomSheet(
+        modifier = modifier.statusBarsPadding(),
+        onDismissRequest = onDismiss,
+        dragHandle = null,
+        contentWindowInsets = { WindowInsets() }
+    ) {
+        content()
+    }
 }
 
 @Preview
