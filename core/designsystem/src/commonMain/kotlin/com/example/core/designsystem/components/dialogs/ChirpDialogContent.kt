0679606071bcd3914b7a59048fa8c6ce2fab91bd
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-17 23:39:00 +0200

    feature(dialogs): add content
    Updated previews

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpDialogContent.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpDialogContent.kt
@@ -1,7 +1,18 @@
 package com.example.core.designsystem.components.dialogs
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.example.core.designsystem.theme.ChirpTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -11,6 +22,18 @@ fun ChirpDialogContent(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface(
+            modifier = modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp)),
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            content()
+        }
+    }
 }
 
 @Preview
@@ -19,7 +42,11 @@ fun PreviewChirpDialogContent() {
     ChirpTheme {
         ChirpDialogContent(
             onDismiss = {},
-            content = {}
+            content = {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Lorem ipsum", modifier = Modifier.align(Alignment.Center))
+                }
+            }
         )
     }
 }
@@ -30,7 +57,11 @@ fun PreviewDarkChirpDialogContent() {
     ChirpTheme(darkTheme = true) {
         ChirpDialogContent(
             onDismiss = {},
-            content = {}
+            content = {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    Text(text = "Lorem ipsum", modifier = Modifier.align(Alignment.Center))
+                }
+            }
         )
     }
 }
