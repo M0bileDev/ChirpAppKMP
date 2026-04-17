eb84028970749b6c0c66bb24c7c6d9cfca9f0863
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-18 00:42:47 +0200

    feature(util): create view model that represents view models scoped to dialogs / bottom sheets
    Represents common pattern of store owner scoped to navigation, activity, fragments (hashmap of ViewModelStore)

---
+++ core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/ScopedStoreRegistryViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.core.presentation.util
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Maintain view model references scoped to dialogs / bottom sheets
+ */
+class ScopedStoreRegistryViewModel : ViewModel() {
+}
