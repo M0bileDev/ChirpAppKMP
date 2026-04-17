f66967ff859aedc3b962c943aecdff559d203ff5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-18 01:16:44 +0200

    feature(util): create logic that produces and provide ViewModelStoreOwner into composables

--- core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/DialogSheetScopedViewModel.kt
+++ core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/DialogSheetScopedViewModel.kt
@@ -3,7 +3,17 @@
 package com.example.core.presentation.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
@@ -13,4 +23,28 @@ fun DialogSheetScopedViewModel(
     scopeId: String = rememberSaveable { Uuid.random().toString() },
     content: @Composable () -> Unit
 ) {
+    val parentStoreOwner = LocalViewModelStoreOwner.current ?: error("No parent store owner found")
+
+    val registry = koinViewModel<ScopedStoreRegistryViewModel>(
+        viewModelStoreOwner = parentStoreOwner
+    )
+
+    var owner by remember { mutableStateOf<ViewModelStoreOwner?>(null) }
+
+    LaunchedEffect(visible, scopeId) {
+        if (visible && owner == null) {
+            owner = object : ViewModelStoreOwner {
+                override val viewModelStore: ViewModelStore
+                    get() = registry.getOrCreate(scopeId)
+            }
+        } else if (!visible && owner != null) {
+            registry.clear(scopeId)
+        }
+    }
+
+    owner?.let { dialogSheetOwner ->
+        CompositionLocalProvider(LocalViewModelStoreOwner provides dialogSheetOwner) {
+            content()
+        }
+    }
 }
