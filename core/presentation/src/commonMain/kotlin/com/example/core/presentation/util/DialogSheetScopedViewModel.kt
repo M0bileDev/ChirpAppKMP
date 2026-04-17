e68ca6ddc9afdb0938b1621da2d901b7e8a2b6bc
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-18 00:35:03 +0200

    feature(utils): add expected arguments

--- core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/DialogSheetScopedViewModel.kt
+++ core/presentation/src/commonMain/kotlin/com/example/core/presentation/util/DialogSheetScopedViewModel.kt
@@ -1,6 +1,16 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package com.example.core.presentation.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
 @Composable
-fun DialogSheetScopedViewModel(){}
+fun DialogSheetScopedViewModel(
+    visible: Boolean,
+    scopeId: String = rememberSaveable { Uuid.random().toString() },
+    content: @Composable () -> Unit
+) {
+}
