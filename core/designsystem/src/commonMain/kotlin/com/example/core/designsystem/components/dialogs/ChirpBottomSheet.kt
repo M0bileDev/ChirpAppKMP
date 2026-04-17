87b73037c4f31d7dc46d14d2b62ffc00de45e094
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-17 23:18:22 +0200

    feature(dialogs): create bottom sheet component

---
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dialogs/ChirpBottomSheet.kt
@@ -0,0 +1,16 @@
+package com.example.core.designsystem.components.dialogs
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChirpBottomSheet(modifier: Modifier = Modifier){}
+
+@Preview
+@Composable
+fun PreviewChirpBottomSheet(){}
+
+@Preview
+@Composable
+fun PreviewDarkChirpBottomSheet(){}
