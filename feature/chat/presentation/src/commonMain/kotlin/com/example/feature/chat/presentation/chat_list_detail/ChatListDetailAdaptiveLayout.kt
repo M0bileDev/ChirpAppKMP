f9adb2b5b6462e745885b73b62fc81f732690fc1
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-19 11:36:50 +0200

    feature(chat_list_detail): provide implementation of PaneScaffold
    Created directive and PaneScaffold navigator (wip)

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
@@ -1,10 +1,36 @@
+@file:OptIn(ExperimentalMaterial3AdaptiveApi::class)
+
 package com.example.feature.chat.presentation.chat_list_detail
 
+import androidx.compose.foundation.background
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.core.designsystem.theme.extended
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun ChatListDetailAdaptiveLayout(
-    modifier: Modifier = Modifier
+    viewModel: ChatListDetailViewModel = koinViewModel<ChatListDetailViewModel>()
 ) {
+    val paneScaffoldDirective = createPaneScaffoldDirectiveByConfigurationAndAdaptiveInfo()
+    val scaffoldNavigator = rememberListDetailPaneScaffoldNavigator(
+        scaffoldDirective = paneScaffoldDirective
+    )
+
+    ListDetailPaneScaffold(
+        modifier = Modifier
+            .background(MaterialTheme.colorScheme.extended.surfaceLower),
+        directive = paneScaffoldDirective,
+        value = scaffoldNavigator.scaffoldValue,
+        listPane = {
+            // TODO: implement list pane
+        },
+        detailPane = {
+            // TODO: implement detail list pane
+        },
+    )
 }
