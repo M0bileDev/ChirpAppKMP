f7d8b0854b04cf0e72d31c86ba2a29e95e8b26b4
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-19 10:29:25 +0200

    feature(chat_list_detail): create component that works as adaptive layout (list view for mobile, list + details view for rest configurations like tablet and desktop)

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
@@ -0,0 +1,10 @@
+package com.example.feature.chat.presentation.chat_list_detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatListDetailAdaptiveLayout(
+    modifier: Modifier = Modifier
+) {
+}
