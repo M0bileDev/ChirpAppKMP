42f3ba50674732d517bafac62502952b35fbf822
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-20 22:55:29 +0200

    feature(presentation): add resources dependency

--- feature/chat/presentation/build.gradle.kts
+++ feature/chat/presentation/build.gradle.kts
@@ -19,6 +19,7 @@ kotlin {
                 implementation(libs.material3.adaptive.layout)
                 implementation(libs.material3.adaptive.navigation)
                 implementation(libs.jetbrains.compose.backhandler)
+                implementation(compose.components.resources)
             }
         }
 
