1bf3af3b1b1e1b069af9c20be3dfae4213f292d4
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-22 22:25:02 +0200

    feature(components): set view attributes
    Renamed file and composable

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/create_chat/components/CreateChatHeaderRow.kt
+++
@@ -1,6 +0,0 @@
-package com.example.feature.chat.presentation.create_chat.components
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun CreateChatHeaderRow(){}
---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/create_chat/components/ManageChatHeaderRow.kt
@@ -0,0 +1,11 @@
+package com.example.feature.chat.presentation.create_chat.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ManageChatHeaderRow(
+    title:String,
+    onCloseClick: () -> Unit,
+    modifier: Modifier = Modifier
+){}
