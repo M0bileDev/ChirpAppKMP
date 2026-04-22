d56eeb45b6522456e20a15c59483f350bed18949
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-22 22:42:53 +0200

    feature(create_chat): create container for the rest of the elements

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/create_chat/CreateChatScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/create_chat/CreateChatScreen.kt
@@ -1,9 +1,17 @@
 package com.example.feature.chat.presentation.create_chat
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.core.presentation.util.clearFocusOnTap
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -23,7 +31,20 @@ fun CreateChatRoot(
 fun CreateChatScreen(
     state: CreateChatState,
     onAction: (CreateChatAction) -> Unit
-) = with(state) {}
+) = with(state) {
+
+
+    Column(
+        modifier = Modifier
+            .clearFocusOnTap()
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .imePadding()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
+
+    }
+}
 
 @Preview
 @Composable
