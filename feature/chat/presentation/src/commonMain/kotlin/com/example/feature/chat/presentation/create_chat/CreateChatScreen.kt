b78a5f22de4269d41ca5b3b5b3ba57739645d271
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-22 22:48:33 +0200

    feature(create_chat): add content for the header

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/create_chat/CreateChatScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/create_chat/CreateChatScreen.kt
@@ -1,17 +1,30 @@
 package com.example.feature.chat.presentation.create_chat
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalDensity
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.create_chat
+import com.example.core.designsystem.components.brand.ChirpHorizontalDivider
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.core.presentation.composableUtil.currentDeviceConfiguration
+import com.example.core.presentation.util.DeviceConfiguration
 import com.example.core.presentation.util.clearFocusOnTap
+import com.example.feature.chat.presentation.components.ManageChatHeaderRow
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -33,6 +46,15 @@ fun CreateChatScreen(
     onAction: (CreateChatAction) -> Unit
 ) = with(state) {
 
+    val isTextFieldFocused by remember { mutableStateOf(false) }
+    val imeHeight = WindowInsets.ime.getBottom(LocalDensity.current)
+    val isKeyboardVisible = imeHeight > 0
+    val configuration = currentDeviceConfiguration()
+
+    val shouldHideHeader =
+        configuration == DeviceConfiguration.MOBILE_LANDSCAPE
+                || (isKeyboardVisible && configuration != DeviceConfiguration.DESKTOP)
+                || isKeyboardVisible
 
     Column(
         modifier = Modifier
@@ -42,7 +64,20 @@ fun CreateChatScreen(
             .imePadding()
             .background(MaterialTheme.colorScheme.surface)
     ) {
-
+        AnimatedVisibility(
+            visible = !shouldHideHeader
+        ) {
+            Column {
+                ManageChatHeaderRow(
+                    modifier = Modifier.fillMaxWidth(),
+                    title = stringResource(Res.string.create_chat),
+                    onCloseClick = {
+                        onAction(CreateChatAction.OnDismissDialog)
+                    }
+                )
+                ChirpHorizontalDivider()
+            }
+        }
     }
 }
 
