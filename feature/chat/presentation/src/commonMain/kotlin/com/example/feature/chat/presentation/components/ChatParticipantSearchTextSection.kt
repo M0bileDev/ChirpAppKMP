cfce463b825cb3d7900d6e27550aa87baf37b28f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-22 22:57:34 +0200

    feature(components): create container for content

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantSearchTextSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantSearchTextSection.kt
@@ -1,8 +1,13 @@
 package com.example.feature.chat.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.core.presentation.util.UiText
 
 @Composable
@@ -15,5 +20,15 @@ fun ChatParticipantSearchTextSection(
     error: UiText? = null,
     onFocusChanged: (Boolean) -> Unit
 ) {
+    Row(
+        modifier = modifier
+            .padding(
+                horizontal = 20.dp,
+                vertical = 16.dp
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
 
+    }
 }
