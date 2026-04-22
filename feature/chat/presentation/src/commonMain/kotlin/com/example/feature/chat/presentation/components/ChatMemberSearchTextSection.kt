6517291be14b90d9db215ae8cb663b0432f4c799
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-22 22:52:16 +0200

    feature(components): create file that contains search member section

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatMemberSearchTextSection.kt
@@ -0,0 +1,11 @@
+package com.example.feature.chat.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatMemberSearchTextSection(
+    modifier: Modifier = Modifier
+) {
+
+}
