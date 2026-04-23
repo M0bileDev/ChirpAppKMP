5252ae0c42bbf1dbea78dbbd4c828049403a6567
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-23 22:41:41 +0200

    feature(components): create chat list item

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantsSelectionSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantsSelectionSection.kt
@@ -1,15 +1,26 @@
 package com.example.feature.chat.presentation.components
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.components.avatar.ChirpAvatarPhoto
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.core.designsystem.theme.extended
+import com.example.core.designsystem.theme.titleXSmall
 import com.example.core.presentation.composableUtil.currentDeviceConfiguration
 import com.example.core.presentation.util.DeviceConfiguration
 import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
@@ -44,6 +55,33 @@ fun ColumnScope.ChatParticipantsSelectionSection(
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ChatParticipantListItem(
+    participantUi: ChatParticipantUi,
+    modifier: Modifier = Modifier
+) = with(participantUi) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        ChirpAvatarPhoto(
+            displayText = initials,
+            imageUrl = imageUrl
+        )
+        Text(
+            text = username,
+            style = MaterialTheme.typography.titleXSmall,
+            color = MaterialTheme.colorScheme.extended.textPrimary,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
