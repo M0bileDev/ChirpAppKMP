b19313066dd2e1c895e7b3f3a1c1cc0946f296ac
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-23 22:30:44 +0200

    feature(components): update scope contract for the composable
    Created conditional height modifier based on device configuration

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantsSelectionSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantsSelectionSection.kt
@@ -1,17 +1,35 @@
 package com.example.feature.chat.presentation.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.core.presentation.composableUtil.currentDeviceConfiguration
+import com.example.core.presentation.util.DeviceConfiguration
 import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ChatParticipantsSelectionSection(
+fun ColumnScope.ChatParticipantsSelectionSection(
     selectedParticipants: List<ChatParticipantUi>,
     modifier: Modifier = Modifier,
     searchResult: ChatParticipantUi? = null
 ) {
+    val deviceConfiguration = currentDeviceConfiguration()
+    val rootHeightModifier = when (deviceConfiguration) {
+        DeviceConfiguration.TABLET_PORTRAIT,
+        DeviceConfiguration.TABLET_LANDSCAPE,
+        DeviceConfiguration.DESKTOP -> {
+            Modifier
+                .animateContentSize()
+                .heightIn(min = 200.dp, max = 300.dp)
+        }
+
+        else -> Modifier.weight(1f)
+    }
 }
 
 @Preview
