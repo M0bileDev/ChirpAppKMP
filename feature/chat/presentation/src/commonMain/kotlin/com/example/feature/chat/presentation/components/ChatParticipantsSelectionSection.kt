260f7292c6999903a16b9dfc76bf9e0cf9f7da0d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-23 22:25:42 +0200

    feature(components): define component's properties

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantsSelectionSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatParticipantsSelectionSection.kt
@@ -3,10 +3,15 @@ package com.example.feature.chat.presentation.components
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-fun ChatParticipantsSelectionSection(modifier: Modifier = Modifier) {
+fun ChatParticipantsSelectionSection(
+    selectedParticipants: List<ChatParticipantUi>,
+    modifier: Modifier = Modifier,
+    searchResult: ChatParticipantUi? = null
+) {
 }
 
 @Preview
