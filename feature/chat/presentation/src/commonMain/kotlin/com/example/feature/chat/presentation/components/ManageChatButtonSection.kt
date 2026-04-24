56f204769ad15d8bfef7b7c41e9a3bfb582ae246
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-24 22:50:37 +0200

    feature(components): add content

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ManageChatButtonSection.kt
@@ -0,0 +1,46 @@
+package com.example.feature.chat.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.components.buttons.ChirpButton
+import com.example.core.designsystem.theme.ChirpTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ManageChatButtonSection(
+    primaryButton: @Composable () -> Unit,
+    secondaryButton: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.End)
+    ) {
+        primaryButton()
+        secondaryButton()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewManageChatButtonRow() {
+    ChirpTheme {
+        ManageChatButtonSection()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDarkManageChatButtonRow() {
+    ChirpTheme(
+        darkTheme = true
+    ) {
+        ManageChatButtonSection()
+    }
+}
