2ae7cab65b18613ed43c2c6d0a5917340c6fd492
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-25 11:58:06 +0200

    feature(chat): create ktor implementation of ChatParticipantService

---
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/KtorChatParticipantService.kt
@@ -0,0 +1,28 @@
+package com.example.feature.chat.data.chat
+
+import com.example.core.data.network.get
+import com.example.core.domain.util.DataError
+import com.example.core.domain.util.Result
+import com.example.core.domain.util.map
+import com.example.feature.chat.data.dto.ChatParticipantDto
+import com.example.feature.chat.data.mappers.toDomain
+import com.example.feature.chat.domain.ChatParticipant
+import com.example.feature.chat.domain.chat.ChatParticipantService
+import io.ktor.client.HttpClient
+
+class KtorChatParticipantService(
+    val httpClient: HttpClient
+) : ChatParticipantService {
+
+    override suspend fun searchParticipant(query: String): Result<ChatParticipant, DataError.Remote> {
+        return httpClient.get<ChatParticipantDto>(
+            route = "/participants",
+            queryParams = mapOf(
+                "query" to query
+            )
+        ).map { dto ->
+            dto.toDomain()
+        }
+    }
+
+}
