6273e449f0592eba5317ad2a2662a2f67df1b64f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-25 11:47:28 +0200

    feature(data): add koin core dependencies

--- feature/chat/data/build.gradle.kts
+++ feature/chat/data/build.gradle.kts
@@ -18,6 +18,7 @@ kotlin {
                 implementation(projects.feature.chat.domain)
                 implementation(projects.feature.chat.database)
                 implementation(libs.bundles.ktor.common)
+                implementation(libs.koin.core)
             }
         }
 
