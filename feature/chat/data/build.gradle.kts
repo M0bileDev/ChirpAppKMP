f6bf547fd3955b024c01d6e45e25a9c0cf47c207
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-25 11:45:52 +0200

    feature(data): add ktor common dependencies

--- feature/chat/data/build.gradle.kts
+++ feature/chat/data/build.gradle.kts
@@ -17,6 +17,7 @@ kotlin {
                 implementation(projects.core.data)
                 implementation(projects.feature.chat.domain)
                 implementation(projects.feature.chat.database)
+                implementation(libs.bundles.ktor.common)
             }
         }
 
