969245242774b5e2e42a67de882cf7a4c2716092
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-26 22:08:56 +0200

    feature(chat): create chat service that contains chat related api calls
    Created api call for chat creation

---
+++ feature/chat/domain/src/commonMain/kotlin/com/example/feature/chat/domain/chat/ChatService.kt
@@ -0,0 +1,11 @@
+package com.example.feature.chat.domain.chat
+
+import com.example.core.domain.util.DataError
+import com.example.core.domain.util.Result
+import com.example.feature.chat.domain.Chat
+
+interface ChatService {
+    suspend fun createChat(
+        otherUserIds: List<String>
+    ): Result<Chat, DataError.Remote>
+}
