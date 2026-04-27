069bbb4b374daa4e020d213c430e7436b8801686
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-27 23:02:39 +0200

    feature(mappers): create chat mapper from domain to ui

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/mappers/ChatMappers.kt
@@ -0,0 +1,14 @@
+package com.example.feature.chat.presentation.mappers
+
+import com.example.feature.chat.domain.Chat
+import com.example.feature.chat.presentation.model.ChatUi
+
+fun Chat.toUi(localParticipantId: String): ChatUi {
+    val (local, other) = participants.partition { it.userId == localParticipantId }
+    return ChatUi(
+        id = id,
+        localParticipant = local.first().toUi(),
+        otherParticipants = other.map { it.toUi() },
+        lastMessage = lastMessage
+    )
+}
