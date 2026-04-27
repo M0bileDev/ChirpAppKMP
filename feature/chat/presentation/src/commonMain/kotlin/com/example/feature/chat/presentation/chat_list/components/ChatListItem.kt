9f434578d6dc578da3a04ad67b46bd710d99c434
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-27 22:35:38 +0200

    feature(components): create selection accent

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListItem.kt
@@ -1,13 +1,18 @@
 package com.example.feature.chat.presentation.chat_list.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.unit.dp
 import com.example.core.designsystem.theme.ChirpTheme
 import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.domain.Chat
@@ -23,13 +28,21 @@ fun ChatListItem(
         if (isSelected) MaterialTheme.colorScheme.surface
         else MaterialTheme.colorScheme.extended.surfaceLower
 
+    val selectedAlpha = if (isSelected) 1f else 0f
+
     Row(
         modifier = modifier
             .height(IntrinsicSize.Min)
             .background(selectedColor)
             .fillMaxWidth()
     ) {
-
+        Box(
+            modifier = Modifier
+                .alpha(selectedAlpha)
+                .background(MaterialTheme.colorScheme.primary)
+                .width(4.dp)
+                .fillMaxHeight()
+        )
     }
 }
 
