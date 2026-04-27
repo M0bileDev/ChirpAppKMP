cbf275664533387fe6904d9cf5a138587e83e22a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-27 22:32:20 +0200

    feature(components): create component container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListItem.kt
@@ -1,8 +1,15 @@
 package com.example.feature.chat.presentation.chat_list.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.domain.Chat
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -12,6 +19,18 @@ fun ChatListItem(
     isSelected: Boolean,
     modifier: Modifier = Modifier
 ) {
+    val selectedColor =
+        if (isSelected) MaterialTheme.colorScheme.surface
+        else MaterialTheme.colorScheme.extended.surfaceLower
+
+    Row(
+        modifier = modifier
+            .height(IntrinsicSize.Min)
+            .background(selectedColor)
+            .fillMaxWidth()
+    ) {
+
+    }
 }
 
 @Preview
