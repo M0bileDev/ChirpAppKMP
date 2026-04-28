10c6c43d99193f0bdf9a4560f31bca479b0d353c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-28 22:49:30 +0200

    feature(components): create components that represents chat header

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ChatHeder.kt
@@ -0,0 +1,8 @@
+package com.example.feature.chat.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatHeader(modifier: Modifier = Modifier) {
+}
