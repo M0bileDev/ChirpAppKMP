be5bf821762f43a6497343f531499997a8d21fac
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 23:09:40 +0200

    feature(components): create previews

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ProfileAvatarSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ProfileAvatarSection.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
 package com.example.feature.chat.presentation.components
 
 import androidx.compose.foundation.BorderStroke
@@ -21,10 +23,14 @@ import chirpappkmp.feature.chat.presentation.generated.resources.logout
 import chirpappkmp.feature.chat.presentation.generated.resources.profile_settings
 import com.example.core.designsystem.components.avatar.ChirpAvatarPhoto
 import com.example.core.designsystem.components.brand.ChirpHorizontalDivider
+import com.example.core.designsystem.theme.ChirpTheme
 import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 import chirpappkmp.core.designsystem.generated.resources.Res as DesignSystemRes
 
 
@@ -102,6 +108,96 @@ fun ProfileAvatarSection(
                     onDismissMenu()
                     onLogoutClick()
                 }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProfileAvatarSection() {
+    ChirpTheme {
+        ProfileAvatarSection(
+            localParticipant = ChatParticipantUi(
+                id = Uuid.random().toString(),
+                username = "Lorem ipsum",
+                initials = "LI"
+            ),
+            isMenuOpen = false,
+            onClick = {},
+            onDismissMenu = {},
+            onProfileSettingsClick = {},
+            onLogoutClick = {},
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewDarkProfileAvatarSection() {
+    ChirpTheme(
+        darkTheme = true
+    ) {
+        ProfileAvatarSection(
+            localParticipant = ChatParticipantUi(
+                id = Uuid.random().toString(),
+                username = "Lorem ipsum",
+                initials = "LI"
+            ),
+            isMenuOpen = false,
+            onClick = {},
+            onDismissMenu = {},
+            onProfileSettingsClick = {},
+            onLogoutClick = {},
+        )
+    }
+}
+
+//There is some preview bug, use interactive mode
+@Preview(showBackground = true, backgroundColor = 0xFF000000)
+@Composable
+fun PreviewProfileAvatarSectionMenuOpen() {
+    ChirpTheme {
+        Box(
+            modifier = Modifier.size(200.dp)
+        ) {
+            ProfileAvatarSection(
+                localParticipant = ChatParticipantUi(
+                    id = Uuid.random().toString(),
+                    username = "Lorem ipsum",
+                    initials = "LI"
+                ),
+                isMenuOpen = true,
+                onClick = {},
+                onDismissMenu = {},
+                onProfileSettingsClick = {},
+                onLogoutClick = {},
+            )
+        }
+    }
+}
+
+//There is some preview bug, use interactive mode
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+fun PreviewDarkProfileAvatarSectionMenuOpen() {
+    ChirpTheme(
+        darkTheme = true
+    ) {
+        Box(
+            modifier = Modifier.size(200.dp)
+        ) {
+            ProfileAvatarSection(
+                localParticipant = ChatParticipantUi(
+                    id = Uuid.random().toString(),
+                    username = "Lorem ipsum2",
+                    initials = "LI2"
+                ),
+                isMenuOpen = true,
+                onClick = {},
+                onDismissMenu = {},
+                onProfileSettingsClick = {},
+                onLogoutClick = {},
             )
         }
     }
