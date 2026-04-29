4646978a2e2bfa222be8a47514e283a814b75916
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 22:25:41 +0200

    feature(components): set component container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
@@ -1,8 +1,10 @@
 package com.example.feature.chat.presentation.chat_list.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.feature.chat.domain.model.ChatParticipant
+import com.example.feature.chat.presentation.components.ChatHeader
 
 @Composable
 fun ChatListHeader(
@@ -14,4 +16,9 @@ fun ChatListHeader(
     onLogoutClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    ChatHeader(
+        modifier = modifier.fillMaxWidth()
+    ) {
+
+    }
 }
