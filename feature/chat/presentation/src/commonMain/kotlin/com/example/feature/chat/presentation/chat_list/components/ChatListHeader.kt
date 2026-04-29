f838d48e5095c85f796b742e9cc18b3bf55ae166
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 22:21:31 +0200

    feature(components): create component that represent chat header in the list view

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
@@ -0,0 +1,10 @@
+package com.example.feature.chat.presentation.chat_list.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatListHeader(
+    modifier: Modifier
+) {
+}
