d7c9e99a4a678cedda131e58b0a9b9fd33d72932
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 22:55:26 +0200

    feature(components): create drop down items

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ProfileAvatarSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ProfileAvatarSection.kt
@@ -1,15 +1,32 @@
 package com.example.feature.chat.presentation.components
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import chirpappkmp.core.designsystem.generated.resources.log_out_icon
+import chirpappkmp.core.designsystem.generated.resources.users_icon
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.logout
+import chirpappkmp.feature.chat.presentation.generated.resources.profile_settings
 import com.example.core.designsystem.components.avatar.ChirpAvatarPhoto
+import com.example.core.designsystem.components.brand.ChirpHorizontalDivider
 import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+import chirpappkmp.core.designsystem.generated.resources.Res as DesignSystemRes
+
 
 @Composable
 fun ProfileAvatarSection(
@@ -39,8 +56,53 @@ fun ProfileAvatarSection(
                 color = MaterialTheme.colorScheme.extended.surfaceOutline
             )
         ) {
-            // TODO: items 
+            DropdownMenuItem(
+                text = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            imageVector = vectorResource(DesignSystemRes.drawable.users_icon),
+                            contentDescription = stringResource(Res.string.profile_settings),
+                            tint = MaterialTheme.colorScheme.extended.textSecondary
+                        )
+                        Text(
+                            text = stringResource(Res.string.profile_settings),
+                            color = MaterialTheme.colorScheme.extended.textSecondary,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                },
+                onClick = {
+                    onDismissMenu()
+                    onProfileSettingsClick()
+                }
+            )
+            ChirpHorizontalDivider()
+            DropdownMenuItem(
+                text = {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(20.dp),
+                            imageVector = vectorResource(DesignSystemRes.drawable.log_out_icon),
+                            contentDescription = stringResource(Res.string.logout),
+                            tint = MaterialTheme.colorScheme.extended.destructiveHover
+                        )
+                        Text(
+                            text = stringResource(Res.string.logout),
+                            color = MaterialTheme.colorScheme.extended.destructiveHover,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                },
+                onClick = {
+                    onDismissMenu()
+                    onLogoutClick()
+                }
+            )
         }
     }
-
 }
