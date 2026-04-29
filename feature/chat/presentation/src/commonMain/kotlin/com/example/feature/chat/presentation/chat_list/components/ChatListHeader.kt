9067e95bb2acc8d639c4216c3c2b59dfc740a148
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 22:24:51 +0200

    feature(components): add composable required arguments

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
@@ -2,9 +2,16 @@ package com.example.feature.chat.presentation.chat_list.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.feature.chat.domain.model.ChatParticipant
 
 @Composable
 fun ChatListHeader(
-    modifier: Modifier
+    localParticipant: ChatParticipant,
+    isUserMenuOpen: Boolean,
+    onUserAvatarClick: () -> Unit,
+    onDismissMenu: () -> Unit,
+    onProfileSettingsClick: () -> Unit,
+    onLogoutClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
 }
