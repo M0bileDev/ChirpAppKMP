5500d794391d6a4af8dfef5b1d93808f8a1fcd02
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 22:45:23 +0200

    feature(components): change argument type
    Renamed argument, added brand avatar composable with dropdown menu

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ProfileAvatarSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/components/ProfileAvatarSection.kt
@@ -1,22 +1,46 @@
 package com.example.feature.chat.presentation.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.feature.chat.domain.model.ChatParticipant
+import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.components.avatar.ChirpAvatarPhoto
+import com.example.core.designsystem.theme.extended
+import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
 
 @Composable
 fun ProfileAvatarSection(
-    localParticipant: ChatParticipant,
+    localParticipant: ChatParticipantUi,
     isMenuOpen: Boolean,
     onClick: () -> Unit,
-    onDismiss: () -> Unit,
+    onDismissMenu: () -> Unit,
     onProfileSettingsClick: () -> Unit,
     onLogoutClick: () -> Unit,
     modifier: Modifier = Modifier
 ) = with(localParticipant) {
     Box(
         modifier = modifier
-    ) {}
+    ) {
+        ChirpAvatarPhoto(
+            displayText = initials,
+            imageUrl = imageUrl,
+            onClick = onClick
+        )
+        //use above component as anchor
+        DropdownMenu(
+            expanded = isMenuOpen,
+            onDismissRequest = onDismissMenu,
+            containerColor = MaterialTheme.colorScheme.surface,
+            border = BorderStroke(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.extended.surfaceOutline
+            )
+        ) {
+            // TODO: items 
+        }
+    }
 
 }
