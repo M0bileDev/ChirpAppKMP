b97bd05573d48c7339e3fe4b3ac71a809d656fe8
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 22:35:59 +0200

    feature(components): create logo section

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
@@ -1,10 +1,21 @@
 package com.example.feature.chat.presentation.chat_list.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import chirpappkmp.core.designsystem.generated.resources.logo_chirp
+import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.domain.model.ChatParticipant
 import com.example.feature.chat.presentation.components.ChatHeader
+import org.jetbrains.compose.resources.vectorResource
+import chirpappkmp.core.designsystem.generated.resources.Res as DesignSystemRes
 
 @Composable
 fun ChatListHeader(
@@ -19,6 +30,21 @@ fun ChatListHeader(
     ChatHeader(
         modifier = modifier.fillMaxWidth()
     ) {
-
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Icon(
+                imageVector = vectorResource(DesignSystemRes.drawable.logo_chirp),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.tertiary
+            )
+            Text(
+                text = "Chirp",
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.extended.textPrimary
+            )
+        }
     }
 }
