2b45ca8511bb9e86fe4719b1fc55b7bcb6833b43
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-29 23:15:09 +0200

    feature(components): add profile avatar section

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListHeader.kt
@@ -2,6 +2,7 @@ package com.example.feature.chat.presentation.chat_list.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -12,14 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import chirpappkmp.core.designsystem.generated.resources.logo_chirp
 import com.example.core.designsystem.theme.extended
-import com.example.feature.chat.domain.model.ChatParticipant
 import com.example.feature.chat.presentation.components.ChatHeader
+import com.example.feature.chat.presentation.components.ProfileAvatarSection
+import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
 import org.jetbrains.compose.resources.vectorResource
 import chirpappkmp.core.designsystem.generated.resources.Res as DesignSystemRes
 
 @Composable
 fun ChatListHeader(
-    localParticipant: ChatParticipant,
+    localParticipant: ChatParticipantUi,
     isUserMenuOpen: Boolean,
     onUserAvatarClick: () -> Unit,
     onDismissMenu: () -> Unit,
@@ -44,6 +46,15 @@ fun ChatListHeader(
                 text = "Chirp",
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.extended.textPrimary
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            ProfileAvatarSection(
+                localParticipant = localParticipant,
+                isMenuOpen = isUserMenuOpen,
+                onClick = onUserAvatarClick,
+                onDismissMenu = onDismissMenu,
+                onProfileSettingsClick = onProfileSettingsClick,
+                onLogoutClick = onLogoutClick
             )
         }
     }
