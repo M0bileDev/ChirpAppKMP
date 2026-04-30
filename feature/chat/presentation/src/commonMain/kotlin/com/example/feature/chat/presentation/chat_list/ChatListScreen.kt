0590aac90ae045339e0566ffba58e2f2699ab77d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-30 23:18:38 +0200

    feature(chat_list): set snackbar host state

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListScreen.kt
@@ -2,8 +2,10 @@
 
 package com.example.feature.chat.presentation.chat_list
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
@@ -13,9 +15,11 @@ fun ChatListRoot(
     viewModel: ChatListViewModel = koinViewModel()
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val snackbarHostState = remember { SnackbarHostState() }
 
     ChatListScreen(
         state = state,
+        snackbarHostState = snackbarHostState,
         onAction = viewModel::onAction
     )
 }
@@ -23,5 +27,6 @@ fun ChatListRoot(
 @Composable
 fun ChatListScreen(
     state: ChatListState,
+    snackbarHostState: SnackbarHostState,
     onAction: (ChatListAction) -> Unit
 ) = with(state) {}
