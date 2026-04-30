e32c0ef2b1dba0357ca6bbc254810ca7daee9014
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-30 23:27:07 +0200

    feature(chat_list): create scaffold container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListScreen.kt
@@ -2,11 +2,25 @@
 
 package com.example.feature.chat.presentation.chat_list
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.create_chat
+import com.example.core.designsystem.components.buttons.ChirpFloatingActionButton
+import com.example.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
 
@@ -29,4 +43,25 @@ fun ChatListScreen(
     state: ChatListState,
     snackbarHostState: SnackbarHostState,
     onAction: (ChatListAction) -> Unit
-) = with(state) {}
+) = with(state) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.extended.surfaceLower,
+        contentWindowInsets = WindowInsets.safeDrawing,
+        floatingActionButton = {
+            ChirpFloatingActionButton(
+                onClick = {
+                    onAction(ChatListAction.OnCreateChatClick)
+                },
+                content = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = stringResource(Res.string.create_chat)
+                    )
+                }
+            )
+        },
+    ) {
+
+    }
+}
