525fbdd48e2bd45f422917c7d22f0f9b73a8ac5d
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-30 23:12:45 +0200

    feature(chat_list): create properties that reflect screen state

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListState.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListState.kt
@@ -1,5 +1,14 @@
 package com.example.feature.chat.presentation.chat_list
 
+import com.example.core.presentation.util.UiText
+import com.example.feature.chat.presentation.model.ChatUi
+import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
+
 data class ChatListState(
-    val arg1: Unit = Unit
+    val chats: List<ChatUi> = emptyList(),
+    val error: UiText? = null,
+    val localParticipant: ChatParticipantUi? = null,
+    val isUserManuOpen: Boolean = false,
+    val showLogoutConfirmation: Boolean = false,
+    val selectedChatId: String? = null
 )
