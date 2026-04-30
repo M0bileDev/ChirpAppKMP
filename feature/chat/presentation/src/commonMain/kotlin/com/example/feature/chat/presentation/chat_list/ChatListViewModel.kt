b3bb353b319aca64bcf4e3d918d28b8b680b6480
m0bileDev <damian.ogorek.1996@gmail.com>
2026-04-30 23:08:52 +0200

    feature(chat_list): create view model that represents screen logic

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListViewModel.kt
@@ -2,18 +2,25 @@ package com.example.feature.chat.presentation.chat_list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.core.domain.auth.SessionStorage
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
-class ChatListViewModel(
-    private val sessionStorage: SessionStorage
-) : ViewModel() {
+class ChatListViewModel : ViewModel() {
+    private var hasLoadedInitialData = false
+    private val _state = MutableStateFlow(ChatListState())
+    val state = _state
+        .onStart {
+            if (!hasLoadedInitialData) {
+                // TODO: init data logic
+                hasLoadedInitialData = true
+            }
+        }.stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ChatListState()
+        )
 
-//    init {
-//        viewModelScope.launch {
-//            delay(5000)
-//            sessionStorage.set(null)
-//        }
-//    }
+    fun onAction(chatListAction: ChatListAction) {}
 }
