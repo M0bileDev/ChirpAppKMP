0e7135af5a2ff8400a67a9a32c66348ec9d5c369
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-01 22:45:26 +0200

    feature(chat_list): add section that represents no chats

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListScreen.kt
@@ -5,6 +5,7 @@ package com.example.feature.chat.presentation.chat_list
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material.icons.Icons
@@ -17,12 +18,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import chirpappkmp.feature.chat.presentation.generated.resources.Res
 import chirpappkmp.feature.chat.presentation.generated.resources.create_chat
 import com.example.core.designsystem.components.buttons.ChirpFloatingActionButton
 import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.presentation.chat_list.components.ChatListHeader
+import com.example.feature.chat.presentation.chat_list.components.EmptyChatSection
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
@@ -86,6 +89,16 @@ fun ChatListScreen(
                     onAction(ChatListAction.OnLogoutClick)
                 },
             )
+            if (chats.isEmpty() && !isLoading) {
+                EmptyChatSection(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = 8.dp
+                        )
+                )
+            }
         }
     }
 }
