0f5df04b12d3c2dca011c32960338394b0740e29
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-01 22:27:39 +0200

    feature(components): create composable that represents empty chat section

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/EmptyChatSection.kt
@@ -0,0 +1,8 @@
+package com.example.feature.chat.presentation.chat_list.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EmptyChatSection(modifier: Modifier = Modifier) {
+}
