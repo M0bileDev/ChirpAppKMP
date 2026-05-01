92e4e0b4e150b02182555a897e8bde235eb3bf83
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-01 22:39:12 +0200

    feature(components): create content

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/EmptyChatSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/EmptyChatSection.kt
@@ -1,19 +1,49 @@
 package com.example.feature.chat.presentation.chat_list.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.empty_chat
+import chirpappkmp.feature.chat.presentation.generated.resources.no_messages
+import chirpappkmp.feature.chat.presentation.generated.resources.no_messages_subtitle
+import com.example.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun EmptyChatSection(modifier: Modifier = Modifier) {
+fun EmptyChatSection(
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Image(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(Res.drawable.empty_chat),
+            contentDescription = stringResource(Res.string.no_messages),
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = stringResource(Res.string.no_messages),
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.extended.textPrimary
+        )
+        Text(
+            text = stringResource(Res.string.no_messages_subtitle),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.extended.textSecondary
+        )
     }
 }
