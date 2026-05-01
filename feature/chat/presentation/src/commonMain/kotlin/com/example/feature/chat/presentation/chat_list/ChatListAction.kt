d6fe4343926777267da0b7f7beff9b2369bd6866
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-01 22:23:00 +0200

    feature(chat_list): add action Profile Settings Click

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListAction.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListAction.kt
@@ -4,6 +4,7 @@ sealed interface ChatListAction {
     data object OnUserAvatarClick : ChatListAction
     data object OnDismissUserMenu : ChatListAction
     data object OnLogoutClick : ChatListAction
+    data object OnProfileSettingsClick : ChatListAction
     data object OnConfirmLogout : ChatListAction
     data object OnDismissLogoutDialog : ChatListAction
     data object OnCreateChatClick : ChatListAction
