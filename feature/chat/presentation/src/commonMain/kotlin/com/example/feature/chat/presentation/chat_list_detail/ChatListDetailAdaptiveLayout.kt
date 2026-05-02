019ffc3f787641f978108974e73edb77f964b07e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-02 23:51:10 +0200

    feature(chat_list_detail): add logic for selected chat

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
@@ -3,17 +3,9 @@
 package com.example.feature.chat.presentation.chat_list_detail
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
 import androidx.compose.material3.adaptive.layout.AnimatedPane
@@ -27,17 +19,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.backhandler.BackHandler
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import chirpappkmp.feature.chat.presentation.generated.resources.Res
-import chirpappkmp.feature.chat.presentation.generated.resources.create_chat
-import com.example.core.designsystem.components.buttons.ChirpFloatingActionButton
 import com.example.core.designsystem.theme.extended
 import com.example.core.presentation.util.DialogSheetScopedViewModel
 import com.example.feature.chat.presentation.chat_list.ChatListRoot
 import com.example.feature.chat.presentation.create_chat.CreateChatRoot
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -67,11 +54,15 @@ fun ChatListDetailAdaptiveLayout(
         listPane = {
             AnimatedPane {
                 ChatListRoot(
-                    onChatClick = TODO(),
-                    onLogout = TODO(),
-                    onCreateChatClick = TODO(),
-                    onProfileSettingsClick = TODO(),
-                    viewModel = TODO()
+                    onChatClick = { chat ->
+                        viewModel.onAction(ChatListDetailAction.OnChatClick(chat.id))
+                        scope.launch {
+                            scaffoldNavigator.navigateTo(ListDetailPaneScaffoldRole.Detail)
+                        }
+                    },
+                    onLogout = { },
+                    onCreateChatClick = {},
+                    onProfileSettingsClick = {},
                 )
             }
         },
