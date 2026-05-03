a15838e54ea0d429b835344140408e36c052abed
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-03 23:24:40 +0200

    feature(model): provide state of other user message

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/model/MessageUi.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/model/MessageUi.kt
@@ -2,6 +2,7 @@ package com.example.feature.chat.presentation.model
 
 import com.example.core.presentation.util.UiText
 import com.example.feature.chat.domain.model.ChatMessageDeliveryStatus
+import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
 
 sealed interface MessageUi {
     data class LocalUserMessage(
@@ -11,4 +12,11 @@ sealed interface MessageUi {
         val canRetry: Boolean,
         val formattedSentAt: UiText
     )
+
+    data class OtherUserMessage(
+        val id: String,
+        val content: String,
+        val formattedSendAt: UiText,
+        val sender: ChatParticipantUi
+    ) : MessageUi
 }
