f98f57d734833cbf9d1af8b19c3c57ae0dc61d22
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-03 23:23:06 +0200

    feature(model): provide state of local user message

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/model/MessageUi.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/model/MessageUi.kt
@@ -1,5 +1,14 @@
 package com.example.feature.chat.presentation.model
 
-sealed interface MessageUi {
+import com.example.core.presentation.util.UiText
+import com.example.feature.chat.domain.model.ChatMessageDeliveryStatus
 
+sealed interface MessageUi {
+    data class LocalUserMessage(
+        val id: String,
+        val content: String,
+        val deliveryStatus: ChatMessageDeliveryStatus,
+        val canRetry: Boolean,
+        val formattedSentAt: UiText
+    )
 }
