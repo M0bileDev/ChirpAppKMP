5c6293dba6eacc7cb79a1cc2e717b1960c1c50f9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-03 23:26:30 +0200

    feature(components): set composable arguments

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageListItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageListItem.kt
@@ -2,9 +2,11 @@ package com.example.feature.chat.presentation.chat_detail.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.feature.chat.presentation.model.MessageUi
 
 @Composable
 fun MessageListItemUi(
+    messageUi: MessageUi,
     modifier: Modifier = Modifier
 ) {
 }
