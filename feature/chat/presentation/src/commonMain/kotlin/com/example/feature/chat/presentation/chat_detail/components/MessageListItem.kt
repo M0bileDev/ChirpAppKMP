2964c56deb4b4ac383b6537cc3be9ee3f47cd927
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-03 23:29:32 +0200

    feature(components): create container view with conditional view references

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageListItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageListItem.kt
@@ -1,5 +1,6 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.feature.chat.presentation.model.MessageUi
@@ -9,4 +10,13 @@ fun MessageListItemUi(
     messageUi: MessageUi,
     modifier: Modifier = Modifier
 ) {
+    Box(
+        modifier = modifier
+    ) {
+        when (messageUi) {
+            is MessageUi.DateSeparator -> TODO()
+            is MessageUi.OtherUserMessage -> TODO()
+            is MessageUi.LocalUserMessage -> TODO()
+        }
+    }
 }
