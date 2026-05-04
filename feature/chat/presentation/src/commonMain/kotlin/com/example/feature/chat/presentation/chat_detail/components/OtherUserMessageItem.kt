2aa5f7a90ee4d8560140f0b62bce43aaded8f3db
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 22:03:41 +0200

    feature(components): create container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/OtherUserMessageItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/OtherUserMessageItem.kt
@@ -1,7 +1,12 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.feature.chat.presentation.model.MessageUi
 
 @Composable
@@ -9,5 +14,11 @@ fun OtherUserMessageItem(
     messageUi: MessageUi.OtherUserMessage,
     modifier: Modifier = Modifier
 ) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
 
+    }
 }
