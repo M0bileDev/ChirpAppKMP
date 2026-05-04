c8b01042fa48e88e6de20ecbcabf48458fb4203a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 22:12:15 +0200

    feature(components): create container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateSeparatorItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateSeparatorItem.kt
@@ -1,6 +1,9 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -8,4 +11,10 @@ fun DateSeparatorItem(
     date: String,
     modifier: Modifier = Modifier
 ) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+    }
 }
