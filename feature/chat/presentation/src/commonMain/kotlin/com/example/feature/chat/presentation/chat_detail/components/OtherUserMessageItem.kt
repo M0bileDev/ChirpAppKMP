cde2c653af95126eb0dd4741be19b56d5cb8933c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 22:07:42 +0200

    feature(components): add content of the view

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/OtherUserMessageItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/OtherUserMessageItem.kt
@@ -7,18 +7,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.components.avatar.ChirpAvatarPhoto
+import com.example.core.designsystem.components.chat.ChirpChatBubble
+import com.example.core.designsystem.components.chat.TrianglePosition
 import com.example.feature.chat.presentation.model.MessageUi
 
 @Composable
 fun OtherUserMessageItem(
     messageUi: MessageUi.OtherUserMessage,
     modifier: Modifier = Modifier
-) {
+) = with(messageUi) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.Bottom,
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-
+        ChirpAvatarPhoto(
+            displayText = sender.initials,
+            imageUrl = sender.imageUrl
+        )
+        ChirpChatBubble(
+            messageContent = content,
+            sender = sender.username,
+            formattedDateTime = formattedSendAt.asString(),
+            trianglePosition = TrianglePosition.LEFT
+        )
     }
 }
