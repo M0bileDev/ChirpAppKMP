8feb4205d478362d59a6ad8b628db8b677748171
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 22:24:53 +0200

    feature(components): create component that represents status of the message

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageStatus.kt
@@ -0,0 +1,12 @@
+package com.example.feature.chat.presentation.chat_detail.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.feature.chat.domain.model.ChatMessageDeliveryStatus
+
+@Composable
+fun MessageStatus(
+    status: ChatMessageDeliveryStatus,
+    modifier: Modifier = Modifier
+) {
+}
