30f85b48c9eca07fd8a47768dbdf696546d6c290
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 22:19:59 +0200

    feature(components): create container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/LocalUserMessageItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/LocalUserMessageItem.kt
@@ -1,7 +1,12 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.feature.chat.presentation.model.MessageUi
 
 @Composable
@@ -9,4 +14,11 @@ fun LocalUserMessageItem(
     messageUi: MessageUi.LocalUserMessage,
     modifier: Modifier = Modifier
 ) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End)
+    ) {
+
+    }
 }
