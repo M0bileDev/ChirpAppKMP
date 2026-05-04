22d2617a3f1accd3b9b06c0dbefb494f10f281a8
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-04 22:15:01 +0200

    feature(components): add content of the view

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateSeparatorItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateSeparatorItem.kt
@@ -2,9 +2,15 @@ package com.example.feature.chat.presentation.chat_detail.components
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.theme.extended
 
 @Composable
 fun DateSeparatorItem(
@@ -15,6 +21,13 @@ fun DateSeparatorItem(
         modifier = modifier.fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-
+        HorizontalDivider(modifier = Modifier.weight(1f))
+        Text(
+            modifier = Modifier.padding(horizontal = 40.dp),
+            text = date,
+            style = MaterialTheme.typography.labelSmall,
+            color = MaterialTheme.colorScheme.extended.textPlaceholder
+        )
+        HorizontalDivider(modifier = Modifier.weight(1f))
     }
 }
