40a5002efcb4e53df326092c83f8d1879083d339
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-05 22:36:51 +0200

    feature(components): add composable argument
    Created item from drop down

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/LocalUserMessageItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/LocalUserMessageItem.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.delete_for_everyone
 import chirpappkmp.feature.chat.presentation.generated.resources.you
 import com.example.core.designsystem.components.chat.ChirpChatBubble
 import com.example.core.designsystem.components.chat.TrianglePosition
@@ -25,6 +29,7 @@ fun LocalUserMessageItem(
     messageUi: MessageUi.LocalUserMessage,
     onMessageLongClick: (MessageUi.LocalUserMessage) -> Unit,
     onDismissMessageMenu: () -> Unit,
+    onDeleteClick: () -> Unit,
     modifier: Modifier = Modifier
 ) = with(messageUi) {
     Row(
@@ -56,8 +61,20 @@ fun LocalUserMessageItem(
                     width = 1.dp,
                     color = MaterialTheme.colorScheme.extended.surfaceOutline
                 )
-            ){
-                // TODO: content
+            ) {
+                DropdownMenuItem(
+                    text = {
+                        Text(
+                            text = stringResource(Res.string.delete_for_everyone),
+                            color = MaterialTheme.colorScheme.extended.destructiveHover,
+                            fontWeight = FontWeight.Medium
+                        )
+                    },
+                    onClick = {
+                        onDismissMessageMenu()
+                        onDeleteClick()
+                    }
+                )
             }
         }
     }
