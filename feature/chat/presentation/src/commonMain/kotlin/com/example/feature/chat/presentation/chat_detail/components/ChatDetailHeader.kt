f1c8c0ebaaa1de9940a57ed6593f7f6ecd7a9c1b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-06 23:01:58 +0200

    feature(components): add arguments

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
@@ -2,8 +2,16 @@ package com.example.feature.chat.presentation.chat_detail.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.feature.chat.presentation.model.ChatUi
 
 @Composable
 fun ChatDetailHeader(
+    chatUi: ChatUi,
+    isChatOptionsDropDownOpen: Boolean,
+    onChatOptionsClick: () -> Unit,
+    onDismissChatOptions: () -> Unit,
+    onManageChatClick: () -> Unit,
+    onLeaveChatClick: () -> Unit,
     modifier: Modifier = Modifier
-) {}
+) {
+}
