6ca19e92124e315feb7631071a4d66b77b495b88
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-06 22:26:41 +0200

    feature(dropdown): create component that represent brand drop down

---
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/dropdown/ChirpDropDownMenu.kt
@@ -0,0 +1,10 @@
+package com.example.core.designsystem.components.dropdown
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChirpDropDownMenu(
+    modifier: Modifier = Modifier
+) {
+}
