0f32774f7eef2414f973c3c84b38f2b5f2020dfb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:07:20 +0200

    feature(components): add container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
@@ -1,5 +1,9 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.feature.chat.presentation.model.ChatUi
@@ -15,5 +19,11 @@ fun ChatDetailHeader(
     onLeaveChatClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
 
+    }
 }
