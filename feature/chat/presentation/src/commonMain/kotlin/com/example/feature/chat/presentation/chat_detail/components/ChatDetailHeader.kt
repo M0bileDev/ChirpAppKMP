73774dfad3948170771bc31f691cfc9291007ce7
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:07:57 +0200

    refactor(components): update container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
@@ -1,11 +1,14 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.feature.chat.presentation.model.ChatUi
 
 @Composable
@@ -22,7 +25,9 @@ fun ChatDetailHeader(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.surface)
+            .background(MaterialTheme.colorScheme.surface),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
 
     }
