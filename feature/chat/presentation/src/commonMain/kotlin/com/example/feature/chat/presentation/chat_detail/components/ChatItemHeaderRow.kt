48722c98a842ebdf615b62d7f4a8ef7ece9abe4f
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:20:03 +0200

    feature(components): add arguments

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatItemHeaderRow.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatItemHeaderRow.kt
@@ -2,9 +2,12 @@ package com.example.feature.chat.presentation.chat_detail.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.feature.chat.presentation.model.ChatUi
 
 @Composable
 fun ChatItemHeaderRow(
+    chatUi: ChatUi,
+    isGroupChat: Boolean,
     modifier: Modifier = Modifier
 ) {
 }
