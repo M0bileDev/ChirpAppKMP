74e907a517e4679ecdec7b7de45663882544f2bb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:51:46 +0200

    feature(components): add container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateChip.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateChip.kt
@@ -1,12 +1,23 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun DateChip(
     date: String,
     modifier: Modifier = Modifier
 ) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(100))
+            .background(color = MaterialTheme.colorScheme.surface)
+    ) {
 
+    }
 }
