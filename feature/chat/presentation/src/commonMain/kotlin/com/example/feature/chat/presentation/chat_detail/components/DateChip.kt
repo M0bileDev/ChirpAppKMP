c490df11ed878e45b10473297483d9e9bc656174
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:51:34 +0200

    feature(components): add argument

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateChip.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateChip.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun DateChip(modifier: Modifier = Modifier) {
+fun DateChip(
+    date: String,
+    modifier: Modifier = Modifier
+) {
 
 }
