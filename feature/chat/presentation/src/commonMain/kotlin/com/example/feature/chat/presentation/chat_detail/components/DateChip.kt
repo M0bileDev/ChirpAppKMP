ecd78b553a4ab0a103da41e229a70b2c53042460
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:52:52 +0200

    feature(components): add modifier

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateChip.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/DateChip.kt
@@ -1,12 +1,14 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun DateChip(
@@ -17,6 +19,11 @@ fun DateChip(
         modifier = Modifier
             .clip(RoundedCornerShape(100))
             .background(color = MaterialTheme.colorScheme.surface)
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.outline,
+                shape = RoundedCornerShape(100)
+            )
     ) {
 
     }
