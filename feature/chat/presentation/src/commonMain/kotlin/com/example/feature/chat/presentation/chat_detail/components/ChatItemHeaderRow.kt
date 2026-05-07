98dd8113e46c8bb9563894735852e9ca7e29dd76
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:19:22 +0200

    feature(components): create component that represents avatars with description

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatItemHeaderRow.kt
@@ -0,0 +1,10 @@
+package com.example.feature.chat.presentation.chat_detail.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ChatItemHeaderRow(
+    modifier: Modifier = Modifier
+) {
+}
