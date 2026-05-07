c365897da443f50f617bd93b74e33f2fa504d7e6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:23:00 +0200

    feature(components): add content

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatItemHeaderRow.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatItemHeaderRow.kt
@@ -1,13 +1,72 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.group_chat
+import chirpappkmp.feature.chat.presentation.generated.resources.you
+import com.example.core.designsystem.components.avatar.ChirpStackedAvatars
+import com.example.core.designsystem.theme.extended
+import com.example.core.designsystem.theme.titleXSmall
 import com.example.feature.chat.presentation.model.ChatUi
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun ChatItemHeaderRow(
     chatUi: ChatUi,
     isGroupChat: Boolean,
     modifier: Modifier = Modifier
-) {
+) = with(chatUi) {
+
+    val chatName = if (isGroupChat) stringResource(Res.string.group_chat)
+    else otherParticipants.first().username
+    val you = stringResource(Res.string.you)
+    val formattedUsernames = remember(otherParticipants) {
+        "$you, " + otherParticipants.joinToString { it.username }
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        ChirpStackedAvatars(
+            avatars = otherParticipants
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(6.dp)
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = chatName,
+                style = MaterialTheme.typography.titleXSmall,
+                color = MaterialTheme.colorScheme.extended.textPrimary,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            )
+            if (isGroupChat) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = formattedUsernames,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+            }
+        }
+    }
 }
