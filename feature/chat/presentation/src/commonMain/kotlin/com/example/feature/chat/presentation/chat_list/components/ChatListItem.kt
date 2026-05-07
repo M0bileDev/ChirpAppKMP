c36e872ef9712e30376453493b7ea49a4981415c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:25:52 +0200

    refactor(components): replace private implementation of header with ChatItemHeaderRow

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListItem.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/components/ChatListItem.kt
@@ -16,8 +16,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.SpanStyle
@@ -26,17 +24,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import chirpappkmp.feature.chat.presentation.generated.resources.Res
-import chirpappkmp.feature.chat.presentation.generated.resources.group_chat
-import chirpappkmp.feature.chat.presentation.generated.resources.you
-import com.example.core.designsystem.components.avatar.ChirpStackedAvatars
 import com.example.core.designsystem.theme.ChirpTheme
 import com.example.core.designsystem.theme.extended
-import com.example.core.designsystem.theme.titleXSmall
 import com.example.feature.chat.domain.model.ChatMessage
+import com.example.feature.chat.presentation.chat_detail.components.ChatItemHeaderRow
 import com.example.feature.chat.presentation.model.ChatUi
 import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
-import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import kotlin.time.Clock
 import kotlin.uuid.ExperimentalUuidApi
@@ -53,19 +46,14 @@ fun ChatListItem(
         else MaterialTheme.colorScheme.extended.surfaceLower
     val selectedAlpha = if (isSelected) 1f else 0f
     val isGroupChat = otherParticipants.size > 1
-    val chatName = if (isGroupChat) stringResource(Res.string.group_chat)
-    else otherParticipants.first().username
-    val you = stringResource(Res.string.you)
-    val formattedUsernames = remember(otherParticipants) {
-        "$you, " + otherParticipants.joinToString { it.username }
-    }
+
     val previewMessage = buildAnnotatedString {
         withStyle(
             style = SpanStyle(
                 fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.extended.textSecondary
             )
-        ){
+        ) {
             append("$lastMessageSenderUsername: ")
         }
         append(lastMessage?.content)
@@ -83,40 +71,11 @@ fun ChatListItem(
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(12.dp)
-            ) {
-                ChirpStackedAvatars(
-                    avatars = otherParticipants
-                )
-                Column(
-                    modifier = Modifier
-                        .weight(1f),
-                    verticalArrangement = Arrangement.spacedBy(6.dp)
-                ) {
-                    Text(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = chatName,
-                        style = MaterialTheme.typography.titleXSmall,
-                        color = MaterialTheme.colorScheme.extended.textPrimary,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1
-                    )
-                    if (isGroupChat) {
-                        Text(
-                            modifier = Modifier.fillMaxWidth(),
-                            text = formattedUsernames,
-                            style = MaterialTheme.typography.bodySmall,
-                            color = MaterialTheme.colorScheme.extended.textPlaceholder,
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1
-                        )
-                    }
-                }
-            }
+            ChatItemHeaderRow(
+                modifier = Modifier.fillMaxWidth(),
+                chatUi = chatUi,
+                isGroupChat = isGroupChat
+            )
             lastMessage?.let {
                 Text(
                     text = previewMessage,
