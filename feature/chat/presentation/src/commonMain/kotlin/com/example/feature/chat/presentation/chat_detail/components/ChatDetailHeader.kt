7656b70fe14d0b62260a90712c42509978990ed9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-07 22:15:48 +0200

    feature(components): create conditional icon

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/ChatDetailHeader.kt
@@ -4,12 +4,22 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import chirpappkmp.core.designsystem.generated.resources.arrow_left_icon
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.go_back
+import com.example.core.designsystem.components.buttons.ChirpIconButton
+import com.example.core.designsystem.theme.extended
 import com.example.feature.chat.presentation.model.ChatUi
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+import chirpappkmp.core.designsystem.generated.resources.Res as DesignSystemRes
 
 @Composable
 fun ChatDetailHeader(
@@ -20,6 +30,7 @@ fun ChatDetailHeader(
     onDismissChatOptions: () -> Unit,
     onManageChatClick: () -> Unit,
     onLeaveChatClick: () -> Unit,
+    onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -29,6 +40,17 @@ fun ChatDetailHeader(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-
+        if (!isDetailPresent) {
+            ChirpIconButton(
+                onClick = onBackClick
+            ) {
+                Icon(
+                    modifier = Modifier.size(20.dp),
+                    imageVector = vectorResource(DesignSystemRes.drawable.arrow_left_icon),
+                    contentDescription = stringResource(Res.string.go_back),
+                    tint = MaterialTheme.colorScheme.extended.textSecondary
+                )
+            }
+        }
     }
 }
