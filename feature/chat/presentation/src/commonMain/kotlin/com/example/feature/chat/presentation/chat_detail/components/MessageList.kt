14b645ce87ae1a19ad87d38b449fb020c409e815
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-08 23:27:14 +0200

    feature(components): create arguments

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageList.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageList.kt
@@ -1,10 +1,16 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.feature.chat.presentation.model.MessageUi
 
 @Composable
 fun MessageList(
+    messages: List<MessageUi>,
+    listState: LazyListState,
+    onMessageLongClick: (MessageUi.LocalUserMessage) -> Unit,
+    onMessageRetryClick: (MessageUi.LocalUserMessage) -> Unit,
     modifier: Modifier = Modifier
 ) {
 }
