cb6dbe176ab2ec327ab3991b59622b9ce61028e1
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-08 23:21:02 +0200

    refactor(textfields): update condition

--- core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/core/designsystem/components/textfields/ChirpMultiLineTextField.kt
@@ -66,7 +66,7 @@ fun ChirpMultiLineTextField(
             onKeyboardAction = { onKeyboardAction() },
             cursorBrush = SolidColor(MaterialTheme.colorScheme.extended.textPrimary),
             decorator = { innerBox ->
-                if (placeholder != null && state.text.isNotEmpty()) {
+                if (placeholder != null && state.text.isEmpty()) {
                     Text(
                         text = placeholder,
                         color = MaterialTheme.colorScheme.extended.textPlaceholder,
