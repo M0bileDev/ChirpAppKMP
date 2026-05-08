f2d36580708a211abe51056982dc83b950ee4552
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-08 22:52:46 +0200

    feature(components): create composable arguments

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageBox.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageBox.kt
@@ -1,10 +1,16 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.feature.chat.domain.model.ConnectionState
 
 @Composable
 fun MessageBox(
+    messageTextFieldState: TextFieldState,
+    isTextInputEnabled: Boolean,
+    connectionState: ConnectionState,
+    onSendClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
 }
