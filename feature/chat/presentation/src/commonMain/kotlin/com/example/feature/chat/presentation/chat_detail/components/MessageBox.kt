4bd05a43652dd16b80e3fd25272d42a6b8f4baa4
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-08 22:58:13 +0200

    feature(components): create content

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageBox.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/components/MessageBox.kt
@@ -1,9 +1,17 @@
 package com.example.feature.chat.presentation.chat_detail.components
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.send_a_message
+import com.example.core.designsystem.components.textfields.ChirpMultiLineTextField
 import com.example.feature.chat.domain.model.ConnectionState
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun MessageBox(
@@ -13,4 +21,14 @@ fun MessageBox(
     onSendClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    ChirpMultiLineTextField(
+        modifier = modifier.padding(4.dp),
+        state = messageTextFieldState,
+        placeholder = stringResource(Res.string.send_a_message),
+        enabled = isTextInputEnabled,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Send
+        ),
+        onKeyboardAction = onSendClick
+    )
 }
