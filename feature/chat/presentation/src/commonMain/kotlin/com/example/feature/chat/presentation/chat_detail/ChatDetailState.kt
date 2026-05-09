fb56397cb2661987eac381a9e07fc686adeb0634
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 22:21:02 +0200

    feature(chat_detail): add chat detail properties

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailState.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailState.kt
@@ -1,9 +1,18 @@
 package com.example.feature.chat.presentation.chat_detail
 
+import androidx.compose.foundation.text.input.TextFieldState
 import com.example.core.presentation.util.UiText
 import com.example.feature.chat.presentation.model.ChatUi
-import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
+import com.example.feature.chat.presentation.model.MessageUi
 
 data class ChatDetailState(
-    val arg0: Unit = Unit
+    val chatUi: ChatUi? = null,
+    val messages: List<MessageUi> = emptyList(),
+    val messageTextFieldState: TextFieldState = TextFieldState(),
+    val canSendMessage: Boolean = false,
+    val isChatLoading: Boolean = false,
+    val isPaginationLoading: Boolean = false,
+    val error: UiText? = null,
+    val paginationError: UiText? = null,
+    val endReached: Boolean = false
 )
