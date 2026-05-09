8516f6fbaf3c963e9ebd2767ccddb1df389f252c
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 22:14:07 +0200

    feature(chat_detail): create state that represents behavior of ChatDetailScreen

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailState.kt
@@ -0,0 +1,9 @@
+package com.example.feature.chat.presentation.chat_detail
+
+import com.example.core.presentation.util.UiText
+import com.example.feature.chat.presentation.model.ChatUi
+import com.example.feature.chat.presentation.type_alias.ChatParticipantUi
+
+data class ChatDetailState(
+    val arg0: Unit = Unit
+)
