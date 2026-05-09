92155dbc102c188140ca0c9f02bf57314fa009f3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 22:49:11 +0200

    feature(chat_detail): create private implementation of DynamicRoundedCorner component
    Conditionally create elevation and shape

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -2,16 +2,20 @@
 
 package com.example.feature.chat.presentation.chat_detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.core.designsystem.theme.extended
@@ -61,5 +65,28 @@ fun ChatDetailScreen(
         ) {
 
         }
+    }
+}
+
+@Composable
+private fun DynamicRoundedCorner(
+    isCornersRounded: Boolean,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val shape = if (isCornersRounded) RoundedCornerShape(16.dp) else RectangleShape
+
+    Box(
+        modifier = modifier
+            .shadow(
+                elevation = if (isCornersRounded) 4.dp else 0.dp,
+                shape = shape
+            )
+            .background(
+                color = MaterialTheme.colorScheme.surface,
+                shape = shape
+            )
+    ) {
+        content()
     }
 }
