d72173d7b5497b58f10c749275491ddd648c4dd9
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 22:35:15 +0200

    feature(chat_detail): create container

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -2,9 +2,17 @@
 
 package com.example.feature.chat.presentation.chat_detail
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.core.designsystem.theme.extended
+import com.example.core.presentation.composableUtil.currentDeviceConfiguration
 import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
 
@@ -25,4 +33,18 @@ fun ChatDetailScreen(
     state: ChatDetailState,
     onAction: (ChatDetailAction) -> Unit
 ) = with(state) {
+    val configuration = currentDeviceConfiguration()
+    val containerColor = if(configuration.isMobile){
+        MaterialTheme.colorScheme.surface
+    }else{
+        MaterialTheme.colorScheme.extended.surfaceLower
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        contentWindowInsets = WindowInsets.safeDrawing,
+        containerColor = containerColor
+    ) {
+
+    }
 }
