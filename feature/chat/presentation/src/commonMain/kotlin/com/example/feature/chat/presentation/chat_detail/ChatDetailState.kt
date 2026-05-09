7898c83117e1557126d479172e732481385d2181
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-09 22:23:34 +0200

    feature(chat_detail): add chat detail properties

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailState.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailState.kt
@@ -2,6 +2,7 @@ package com.example.feature.chat.presentation.chat_detail
 
 import androidx.compose.foundation.text.input.TextFieldState
 import com.example.core.presentation.util.UiText
+import com.example.feature.chat.domain.model.ConnectionState
 import com.example.feature.chat.presentation.model.ChatUi
 import com.example.feature.chat.presentation.model.MessageUi
 
@@ -14,5 +15,14 @@ data class ChatDetailState(
     val isPaginationLoading: Boolean = false,
     val error: UiText? = null,
     val paginationError: UiText? = null,
-    val endReached: Boolean = false
+    val endReached: Boolean = false,
+    val bannerState: BannerState = BannerState(),
+    val isChatOptionsOpen: Boolean = false,
+    val isNearBottom: Boolean = false,
+    val connectionState: ConnectionState = ConnectionState.DISCONNECTED
+)
+
+data class BannerState(
+    val formattedDate: UiText? = null,
+    val isVisible: Boolean = false
 )
