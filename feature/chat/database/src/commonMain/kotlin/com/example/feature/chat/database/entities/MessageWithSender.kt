b056e416e5b58c26a9208be7678d0378def86fa6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-12 22:47:02 +0200

    feature(entities): create model that represents message with sender

---
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/entities/MessageWithSender.kt
@@ -0,0 +1,15 @@
+package com.example.feature.chat.database.entities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+
+data class MessageWithSender(
+    @Embedded
+    val message: ChatMessageEntity,
+    @Relation(
+        parentColumn = "senderId",
+        entityColumn = "userId"
+    )
+    val sender: ChatParticipantEntity
+)
