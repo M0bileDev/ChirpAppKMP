5c3410fc7faf36f3ceae0315b67bab0cd7ee5c35
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-13 22:49:57 +0200

    feature(dao): create function that upsert participant

---
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatParticipantDao.kt
@@ -0,0 +1,12 @@
+package com.example.feature.chat.database.dao
+
+import androidx.room.Dao
+import androidx.room.Upsert
+import com.example.feature.chat.database.entities.ChatParticipantEntity
+
+@Dao
+interface ChatParticipantDao {
+
+    @Upsert
+    suspend fun upsertParticipant(participant: ChatParticipantEntity)
+}
