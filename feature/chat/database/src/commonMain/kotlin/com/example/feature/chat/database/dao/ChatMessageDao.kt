82249f3228704416bfc546f88ddd8fcf5526b213
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-13 22:35:40 +0200

    feature(dao): create function that upsert (insert or update) message entity

---
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatMessageDao.kt
@@ -0,0 +1,13 @@
+package com.example.feature.chat.database.dao
+
+import androidx.room.Dao
+import androidx.room.Upsert
+import com.example.feature.chat.database.entities.ChatMessageEntity
+
+@Dao
+interface ChatMessageDao {
+
+    @Upsert
+    suspend fun upsertMessage(chatMessageEntity: ChatMessageEntity)
+
+}
