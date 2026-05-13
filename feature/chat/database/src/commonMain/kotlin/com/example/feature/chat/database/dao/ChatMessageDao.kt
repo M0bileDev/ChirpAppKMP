2137a85bfb2273b79e623550c9460d1c13fdbad8
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-13 22:46:33 +0200

    feature(dao): create function that returns message by id

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatMessageDao.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatMessageDao.kt
@@ -1,7 +1,6 @@
 package com.example.feature.chat.database.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.example.feature.chat.database.entities.ChatMessageEntity
@@ -17,11 +16,14 @@ interface ChatMessageDao {
     suspend fun upsertMessages(messages: List<ChatMessageEntity>)
 
     @Query("DELETE FROM chatmessageentity WHERE messageId = :messageId")
-    suspend fun deleteMessageById(messageId : String)
+    suspend fun deleteMessageById(messageId: String)
 
     @Query("DELETE FROM chatmessageentity WHERE messageId IN (:messageIds)")
     suspend fun deleteMessagesByIds(messageIds: List<String>)
 
     @Query("SELECT * FROM chatmessageentity WHERE chatId = :chatId ORDER BY timestamp DESC")
-    fun getMessagesByChatId(chatId: String) : Flow<List<ChatMessageEntity>>
+    fun getMessagesByChatId(chatId: String): Flow<List<ChatMessageEntity>>
+
+    @Query("SELECT * FROM chatmessageentity WHERE messageId = :messageId")
+    suspend fun getMessageById(messageId: String): ChatMessageEntity?
 }
