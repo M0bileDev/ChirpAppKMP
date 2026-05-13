821f9102fcf844c70da625c728aa60906e21d434
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-13 22:39:19 +0200

    feature(dao): create function that removes message by id

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatMessageDao.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatMessageDao.kt
@@ -1,6 +1,8 @@
 package com.example.feature.chat.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
 import androidx.room.Upsert
 import com.example.feature.chat.database.entities.ChatMessageEntity
 
@@ -12,5 +14,8 @@ interface ChatMessageDao {
 
     @Upsert
     suspend fun upsertMessages(messages: List<ChatMessageEntity>)
+
+    @Query("DELETE FROM chatmessageentity WHERE messageId = :messageId")
+    suspend fun deleteMessageById(messageId : String)
 
 }
