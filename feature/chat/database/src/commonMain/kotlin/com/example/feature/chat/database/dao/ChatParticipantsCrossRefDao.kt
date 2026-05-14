cb84ae2a1a6ad05030b69ae49fed186975a928bb
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-14 22:14:43 +0200

    feature(dao): create function that returns all participants in chat

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatParticipantsCrossRefDao.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/dao/ChatParticipantsCrossRefDao.kt
@@ -14,17 +14,24 @@ interface ChatParticipantsCrossRefDao {
     @Query("SELECT userId FROM chatparticipantcrossref WHERE chatId = :chatId AND isActive = 1")
     suspend fun getActiveParticipantIdsByChat(chatId: String): List<String>
 
-    @Query("""
+    @Query(
+        """
         UPDATE chatparticipantcrossref
         SET isActive = 0
         WHERE chatId = :chatId AND userId IN (:userIds)
-    """)
+    """
+    )
     suspend fun markParticipantsAsInactive(chatId: String, userIds: List<String>)
 
-    @Query("""
+    @Query(
+        """
         UPDATE chatparticipantcrossref
         SET isActive = 1
         WHERE chatId = :chatId AND userId IN (:userIds)
-    """)
+    """
+    )
     suspend fun markParticipantsAsActive(chatId: String, userIds: List<String>)
+
+    @Query("SELECT userId FROM chatparticipantcrossref")
+    suspend fun getParticipantIdsByChat(chatId: String): List<String>
 }
