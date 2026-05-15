94d7ace68164539a9c1a522adbc96a9e44e0ccad
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-16 00:12:31 +0200

    feature(di): define platform specific chat data module

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
@@ -4,9 +4,12 @@ import com.example.feature.chat.data.chat.KtorChatParticipantService
 import com.example.feature.chat.data.chat.KtorChatService
 import com.example.feature.chat.domain.chat.ChatParticipantService
 import com.example.feature.chat.domain.chat.ChatService
+import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+
+expect val platformChatDataModule: Module
 
 val chatDataModule = module {
     singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
