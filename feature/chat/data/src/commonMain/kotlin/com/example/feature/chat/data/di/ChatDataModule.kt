24db9b574b24d44351242c74f8019e8b0c26cc7b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-16 00:15:24 +0200

    feature(di): include platform specific module
    Registered singleton for database factory

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
@@ -1,7 +1,9 @@
 package com.example.feature.chat.data.di
 
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.example.feature.chat.data.chat.KtorChatParticipantService
 import com.example.feature.chat.data.chat.KtorChatService
+import com.example.feature.chat.database.ChirpDatabaseFactory
 import com.example.feature.chat.domain.chat.ChatParticipantService
 import com.example.feature.chat.domain.chat.ChatService
 import org.koin.core.module.Module
@@ -12,6 +14,14 @@ import org.koin.dsl.module
 expect val platformChatDataModule: Module
 
 val chatDataModule = module {
+    includes(platformChatDataModule)
+
     singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
     singleOf(::KtorChatService) bind ChatService::class
+    single {
+        get<ChirpDatabaseFactory>()
+            .create()
+            .setDriver(BundledSQLiteDriver())
+            .build()
+    }
 }
