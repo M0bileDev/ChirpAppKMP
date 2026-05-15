623a27a598043cd2a8cfc4c459cb524542ff0154
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 23:36:05 +0200

    feature(database): define dao properties
    Added database name

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/ChirpChatDatabase.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/ChirpChatDatabase.kt
@@ -2,6 +2,10 @@ package com.example.feature.chat.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.feature.chat.database.dao.ChatDao
+import com.example.feature.chat.database.dao.ChatMessageDao
+import com.example.feature.chat.database.dao.ChatParticipantDao
+import com.example.feature.chat.database.dao.ChatParticipantsCrossRefDao
 import com.example.feature.chat.database.entities.ChatEntity
 import com.example.feature.chat.database.entities.ChatMessageEntity
 import com.example.feature.chat.database.entities.ChatParticipantCrossRef
@@ -22,4 +26,12 @@ import com.example.feature.chat.database.view.LastMessageView
 )
 abstract class ChirpChatDatabase : RoomDatabase() {
 
+    abstract val chatDao: ChatDao
+    abstract val chatParticipantDao: ChatParticipantDao
+    abstract val chatMessageDao: ChatMessageDao
+    abstract val chatParticipantsCrossRefDao: ChatParticipantsCrossRefDao
+
+    companion object {
+        const val DB_NAME = "chirp.db"
+    }
 }
