5375fc3248bc98c43bcc5040c2cfd25e812fcb2b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-16 00:12:58 +0200

    feature(di): provide android specific chat data module

---
+++ feature/chat/data/src/androidMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.android.kt
@@ -0,0 +1,10 @@
+package com.example.feature.chat.data.di
+
+import com.example.feature.chat.database.ChirpDatabaseFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+actual val platformChatDataModule: Module = module {
+    single { ChirpDatabaseFactory(androidContext()) }
+}
