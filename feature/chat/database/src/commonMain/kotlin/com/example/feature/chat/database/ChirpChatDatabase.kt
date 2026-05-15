6a8160ce131ae3fd7b469525aecccb7c6f78e947
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 23:32:42 +0200

    feature(database): define entities and version

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/ChirpChatDatabase.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/ChirpChatDatabase.kt
@@ -1,7 +1,21 @@
 package com.example.feature.chat.database
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.feature.chat.database.entities.ChatEntity
+import com.example.feature.chat.database.entities.ChatMessageEntity
+import com.example.feature.chat.database.entities.ChatParticipantCrossRef
+import com.example.feature.chat.database.entities.ChatParticipantEntity
 
+@Database(
+    entities = [
+        ChatEntity::class,
+        ChatParticipantEntity::class,
+        ChatMessageEntity::class,
+        ChatParticipantCrossRef::class
+    ],
+    version = 1
+)
 abstract class ChirpChatDatabase : RoomDatabase() {
 
 }
