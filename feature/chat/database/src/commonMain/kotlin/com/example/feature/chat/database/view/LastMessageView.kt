6cfb0c0c431f00b6e02525a98dd425bdd3641257
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-15 23:24:37 +0200

    feature(view): create query that be up to date for each action on chatmessageentity table

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/view/LastMessageView.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/view/LastMessageView.kt
@@ -1,5 +1,19 @@
 package com.example.feature.chat.database.view
 
+import androidx.room.DatabaseView
+
+@DatabaseView(
+    viewName = "last_message_per_chat_view",
+    value = """
+        SELECT m1.*
+        FROM chatmessageentity AS m1
+        JOIN (
+            SELECT chatId, MAX(timestamp) AS max_timestamp
+            FROM chatmessageentity
+            GROUP BY chatId
+        ) AS m2 ON m1.chatId = m2.chatId AND m1.timestamp = m2.max_timestamp
+    """
+)
 data class LastMessageView(
     val messageId: String,
     val chatId: String,
