c06ecdb83cc9050e40924d3bfbc6d44b83924f2a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-16 23:55:32 +0200

    refactor(entities): rename property

--- feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/entities/ChatParticipantEntity.kt
+++ feature/chat/database/src/commonMain/kotlin/com/example/feature/chat/database/entities/ChatParticipantEntity.kt
@@ -7,6 +7,6 @@ import androidx.room.PrimaryKey
 data class ChatParticipantEntity(
     @PrimaryKey
     val userId: String,
-    val userName: String,
+    val username: String,
     val profilePictureUrl: String?
 )
