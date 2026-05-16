28bdaec2ec54800780c702ee84abe299dc566a18
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-17 00:00:15 +0200

    feature(mappers): create function that map from entity to domain

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/mappers/ChatMappers.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/mappers/ChatMappers.kt
@@ -1,14 +1,24 @@
 package com.example.feature.chat.data.mappers
 
 import com.example.feature.chat.data.dto.ChatDto
+import com.example.feature.chat.database.entities.ChatWithParticipants
 import com.example.feature.chat.domain.model.Chat
 import kotlin.time.Instant
 
-fun ChatDto.toDomain() : Chat{
+fun ChatDto.toDomain(): Chat {
     return Chat(
         id = id,
         participants = participants.map { it.toDomain() },
         lastActivityAt = Instant.parse(lastActivityAt),
         lastMessage = lastMessage?.toDomain()
+    )
+}
+
+fun ChatWithParticipants.toDomain(): Chat {
+    return Chat(
+        id = chat.chatId,
+        participants = participants.map { it.toDomain() },
+        lastActivityAt = Instant.fromEpochMilliseconds(chat.lastActivityAt),
+        lastMessage = lastMessage.toDomain()
     )
 }
