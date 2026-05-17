232dadd82794fd77fc84c28add4a53ed09c0dd22
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-17 22:21:47 +0200

    feature(chat): provide getChats implementation

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/OfflineFirstChatRepository.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/OfflineFirstChatRepository.kt
@@ -2,11 +2,13 @@ package com.example.feature.chat.data.chat
 
 import com.example.core.domain.util.DataError
 import com.example.core.domain.util.Result
+import com.example.feature.chat.data.mappers.toDomain
 import com.example.feature.chat.database.ChirpChatDatabase
 import com.example.feature.chat.domain.chat.ChatRepository
 import com.example.feature.chat.domain.chat.ChatService
 import com.example.feature.chat.domain.model.Chat
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class OfflineFirstChatRepository(
     private val chatService: ChatService,
@@ -14,7 +16,13 @@ class OfflineFirstChatRepository(
 ) : ChatRepository {
 
     override fun getChats(): Flow<List<Chat>> {
-        TODO("Not yet implemented")
+        return chirpChatDatabase
+            .chatDao
+            .getChatsWithActiveParticipants()
+            .map { chatWithParticipants ->
+                chatWithParticipants.map { it.toDomain() }
+            }
+
     }
 
     override suspend fun fetchChats(): Result<List<Chat>, DataError.Remote> {
