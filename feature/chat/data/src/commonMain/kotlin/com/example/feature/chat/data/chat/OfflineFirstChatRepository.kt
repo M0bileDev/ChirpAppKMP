8967ea0e515d41666841d8d083e08114e39e3029
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-17 22:57:53 +0200

    feature(chat): provide fetchChats implementation

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/OfflineFirstChatRepository.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/OfflineFirstChatRepository.kt
@@ -2,8 +2,12 @@ package com.example.feature.chat.data.chat
 
 import com.example.core.domain.util.DataError
 import com.example.core.domain.util.Result
+import com.example.core.domain.util.onSuccess
 import com.example.feature.chat.data.mappers.toDomain
+import com.example.feature.chat.data.mappers.toEntity
+import com.example.feature.chat.data.mappers.toLastMessageView
 import com.example.feature.chat.database.ChirpChatDatabase
+import com.example.feature.chat.database.entities.ChatWithParticipants
 import com.example.feature.chat.domain.chat.ChatRepository
 import com.example.feature.chat.domain.chat.ChatService
 import com.example.feature.chat.domain.model.Chat
@@ -26,6 +30,30 @@ class OfflineFirstChatRepository(
     }
 
     override suspend fun fetchChats(): Result<List<Chat>, DataError.Remote> {
-        TODO("Not yet implemented")
+        return chatService
+            .getChats()
+            .onSuccess { chats ->
+                saveChats(chats)
+            }
+    }
+
+    private suspend fun saveChats(chats: List<Chat>) {
+        val chatsWithParticipants = chats.map { chat ->
+            ChatWithParticipants(
+                chat = chat.toEntity(),
+                participants = chat.participants.map { participant -> participant.toEntity() },
+                lastMessage = chat.lastMessage?.toLastMessageView()
+            )
+        }
+
+        with(chirpChatDatabase) {
+            chatDao
+                .upsertChatsWithParticipantsAndCrossRefs(
+                    chatsWithParticipants = chatsWithParticipants,
+                    participantDao = chatParticipantDao,
+                    crossRefDao = chatParticipantsCrossRefDao,
+                    messageDao = chatMessageDao
+                )
+        }
     }
 }
