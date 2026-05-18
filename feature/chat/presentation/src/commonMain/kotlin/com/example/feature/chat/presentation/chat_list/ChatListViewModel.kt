f48fd645b353e3ac822a22a5b48aac2fe05f7da3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-18 22:46:46 +0200

    feature(chat_list): add arguments
    Added logic that combine flows from state, repository and storage

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list/ChatListViewModel.kt
@@ -2,15 +2,33 @@ package com.example.feature.chat.presentation.chat_list
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.core.domain.auth.SessionStorage
+import com.example.feature.chat.domain.chat.ChatRepository
+import com.example.feature.chat.presentation.mappers.toUi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
-class ChatListViewModel : ViewModel() {
+class ChatListViewModel(
+    chatRepository: ChatRepository,
+    sessionStorage: SessionStorage
+) : ViewModel() {
     private var hasLoadedInitialData = false
     private val _state = MutableStateFlow(ChatListState())
-    val state = _state
+    val state = combine(
+        _state,
+        chatRepository.getChats(),
+        sessionStorage.observeAuthInfo()
+    ) { currentState, chats, authInfo ->
+        if (authInfo == null) return@combine ChatListState()
+
+        currentState.copy(
+            chats = chats.map { it.toUi(authInfo.user.id) },
+            localParticipant = authInfo.user.toUi()
+        )
+    }
         .onStart {
             if (!hasLoadedInitialData) {
                 // TODO: init data logic
