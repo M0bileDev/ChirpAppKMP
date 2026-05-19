4171bf89f81cf64378d50706d5fc24cd8ecba3b4
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-19 22:40:00 +0200

    feature(chat): define function that returns flow of chat with information about senders and messages

--- feature/chat/domain/src/commonMain/kotlin/com/example/feature/chat/domain/chat/ChatRepository.kt
+++ feature/chat/domain/src/commonMain/kotlin/com/example/feature/chat/domain/chat/ChatRepository.kt
@@ -5,10 +5,12 @@ import com.example.core.domain.util.DataError
 import com.example.core.domain.util.EmptyResult
 import com.example.core.domain.util.Result
 import com.example.feature.chat.domain.model.Chat
+import com.example.feature.chat.domain.model.ChatInfo
 import kotlinx.coroutines.flow.Flow
 
 interface ChatRepository {
     fun getChats(): Flow<List<Chat>>
     suspend fun fetchChats(): Result<List<Chat>, DataError.Remote>
-    suspend fun fetchChatById(chatId:String): EmptyResult<DataError.Remote>
+    suspend fun fetchChatById(chatId: String): EmptyResult<DataError.Remote>
+    fun getChatInfoById(chatId: String): Flow<ChatInfo>
 }
