68f7b4542bfc3c3f4ae58bf2c98c3ae0260c5a51
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-19 22:53:34 +0200

    feature(mappers): create mapper for MessageWithSender entity to domain

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/mappers/ChatMappers.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/mappers/ChatMappers.kt
@@ -2,13 +2,16 @@ package com.example.feature.chat.data.mappers
 
 import com.example.feature.chat.data.dto.ChatDto
 import com.example.feature.chat.database.entities.ChatEntity
-import com.example.feature.chat.database.entities.ChatInfoEntity
 import com.example.feature.chat.database.entities.ChatWithParticipants
+import com.example.feature.chat.database.entities.MessageWithSender
 import com.example.feature.chat.domain.model.Chat
-import com.example.feature.chat.domain.model.ChatInfo
 import com.example.feature.chat.domain.model.ChatMessage
+import com.example.feature.chat.domain.model.ChatMessageDeliveryStatus
 import com.example.feature.chat.domain.model.ChatParticipant
 import kotlin.time.Instant
+
+typealias DataMessageWithSender = MessageWithSender
+typealias DomainMessageWithSender = com.example.feature.chat.domain.model.MessageWithSender
 
 fun ChatDto.toDomain(): Chat {
     return Chat(
@@ -47,3 +50,10 @@ fun ChatEntity.toDomain(
     )
 }
 
+fun DataMessageWithSender.toDomain(): DomainMessageWithSender {
+    return DomainMessageWithSender(
+        message = message.toDomain(),
+        sender = sender.toDomain(),
+        status = ChatMessageDeliveryStatus.valueOf(this.message.deliveryStatus)
+    )
+}
