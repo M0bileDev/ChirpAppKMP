b0f5125178bf908e993dbada591fe42e6be7447a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-22 23:03:12 +0200

    refactor(chat_list_detail): rename function

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_list_detail/ChatListDetailAdaptiveLayout.kt
@@ -22,7 +22,7 @@ import com.example.core.designsystem.theme.extended
 import com.example.core.presentation.util.DialogSheetScopedViewModel
 import com.example.feature.chat.presentation.chat_detail.ChatDetailRoot
 import com.example.feature.chat.presentation.chat_list.ChatListRoot
-import com.example.feature.chat.presentation.create_chat.ManageChatRoot
+import com.example.feature.chat.presentation.create_chat.CreateChatRoot
 import kotlinx.coroutines.launch
 import org.koin.compose.viewmodel.koinViewModel
 
@@ -98,7 +98,7 @@ fun ChatListDetailAdaptiveLayout(
     DialogSheetScopedViewModel(
         visible = sharedState.dialogState is DialogState.CreateChat
     ) {
-        ManageChatRoot(
+        CreateChatRoot(
             onDismiss = {
                 viewModel.onAction(ChatListDetailAction.OnDismissCurrentDialog)
             },
