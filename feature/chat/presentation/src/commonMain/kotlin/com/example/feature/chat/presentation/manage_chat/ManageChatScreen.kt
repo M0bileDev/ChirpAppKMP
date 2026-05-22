bc23d025bb65dab3cc08c2de4080566244b1fd69
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-22 23:05:30 +0200

    feature(manage_chat): create screen that represents root composable for manage chat screen

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatScreen.kt
@@ -0,0 +1,10 @@
+package com.example.feature.chat.presentation.manage_chat
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ManageChatRoot(
+    modifier: Modifier = Modifier
+) {
+}
