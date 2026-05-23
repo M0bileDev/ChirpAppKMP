be7e55b55236c46bfbf98858a5ab3a296c0da69e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-23 22:45:45 +0200

    feature(manage_chat): add content that represents ManageChatScreen

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatScreen.kt
@@ -1,10 +1,38 @@
 package com.example.feature.chat.presentation.manage_chat
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.chat_members
+import chirpappkmp.feature.chat.presentation.generated.resources.save
+import com.example.core.designsystem.components.dialogs.ChirpAdaptiveDialogSheetLayout
+import com.example.feature.chat.presentation.components.ManageChatScreen
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun ManageChatRoot(
-    viewModel: ManageChatViewModel = koinViewModel()
+    onDismiss: () -> Unit,
+    viewModel: ManageChatViewModel = koinViewModel(),
 ) {
+
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    ChirpAdaptiveDialogSheetLayout(
+        onDismiss = onDismiss,
+    ) {
+        ManageChatScreen(
+            state = state,
+            title = stringResource(Res.string.chat_members),
+            primaryButtonText = stringResource(Res.string.save),
+            onAction = { action ->
+                when (action) {
+                    ManageChatAction.OnDismissDialog -> onDismiss()
+                    else -> Unit
+                }
+                viewModel.onAction(action)
+            }
+        )
+    }
 }
