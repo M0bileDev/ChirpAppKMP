5021477ae22eba1145ab64ad9c733a223a64a286
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-23 22:28:21 +0200

    feature(manage_chat): create screen state flow
    Created function that handles chat actions

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.feature.chat.presentation.manage_chat
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class ManageChatViewModel : ViewModel() {
 
+    private val _state = MutableStateFlow(ManageChatState())
+    val state = _state.asStateFlow()
+
+    fun onAction(manageChatAction: ManageChatAction) {}
 }
