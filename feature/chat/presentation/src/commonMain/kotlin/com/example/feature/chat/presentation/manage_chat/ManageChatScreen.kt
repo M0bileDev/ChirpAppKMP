be834ca59149e1d2381f036f44d0ebc00b56831a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-23 22:58:05 +0200

    feature(manage_chat): add function type argument

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatScreen.kt
@@ -14,6 +14,7 @@ import org.koin.compose.viewmodel.koinViewModel
 @Composable
 fun ManageChatRoot(
     onDismiss: () -> Unit,
+    onMembersAdded: () -> Unit,
     viewModel: ManageChatViewModel = koinViewModel(),
 ) {
 
