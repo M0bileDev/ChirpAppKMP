a980ad9ffb7eec333918635560a6dfd96262e36a
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-23 22:57:50 +0200

    feature(manage_chat): create channel properties that represents view model events emission

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
@@ -1,10 +1,15 @@
 package com.example.feature.chat.presentation.manage_chat
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class ManageChatViewModel : ViewModel() {
+
+    private val _eventChannel = Channel<ManageChatEvent>()
+    val events = _eventChannel.receiveAsFlow()
 
     private val _state = MutableStateFlow(ManageChatState())
     val state = _state.asStateFlow()
