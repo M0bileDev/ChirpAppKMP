56c3a2ed5d12e7a7d28df65b25967c6bf5034a42
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-23 23:41:56 +0200

    feature(manage_chat): add repository property

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
@@ -1,12 +1,15 @@
 package com.example.feature.chat.presentation.manage_chat
 
 import androidx.lifecycle.ViewModel
+import com.example.feature.chat.domain.chat.ChatRepository
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 
-class ManageChatViewModel : ViewModel() {
+class ManageChatViewModel(
+    private val chatRepository: ChatRepository
+) : ViewModel() {
 
     private val _eventChannel = Channel<ManageChatEvent>()
     val events = _eventChannel.receiveAsFlow()
