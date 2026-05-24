b318038a2dc9f2a41174b8a2664a7b1db9be5aac
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-24 22:18:32 +0200

    feature(manage_chat): create implementation that represents logic of OnAddClick

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.feature.chat.presentation.manage_chat
 
+import androidx.compose.foundation.text.input.clearText
 import androidx.lifecycle.ViewModel
 import com.example.feature.chat.domain.chat.ChatRepository
 import kotlinx.coroutines.channels.Channel
@@ -20,9 +21,33 @@ class ManageChatViewModel(
 
     fun onAction(manageChatAction: ManageChatAction) {
         when (manageChatAction) {
-            ManageChatAction.OnAddClick -> TODO()
+            ManageChatAction.OnAddClick -> addParticipantToChat()
             is ManageChatAction.ChatParticipantsAction.OnSelectChat -> onSelectChat(manageChatAction.chatId)
             else -> Unit
+        }
+    }
+
+    private fun addParticipantToChat() = with(state.value) {
+        currentSearchResult?.let { participantFromSearch ->
+            val isAlreadySelected = selectedChatParticipants.any {
+                it.id == participantFromSearch.id
+            }
+            val isAlreadyInChat = existingChatParticipants.any {
+                it.id == participantFromSearch.id
+            }
+
+            if (isAlreadySelected || isAlreadyInChat) return@with
+
+            val updatedParticipants = selectedChatParticipants + participantFromSearch
+            _state.update {
+                it.copy(
+                    selectedChatParticipants = updatedParticipants,
+                    canAddParticipant = false,
+                    currentSearchResult = null
+                )
+            }.also {
+                state.value.queryTextState.clearText()
+            }
         }
     }
 
