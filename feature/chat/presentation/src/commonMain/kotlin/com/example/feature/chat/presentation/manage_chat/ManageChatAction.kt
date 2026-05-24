639312b901338e46da820d70bd3b5088a4b0b218
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-24 22:06:35 +0200

    refactor(manage_chat): remove action

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatAction.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/manage_chat/ManageChatAction.kt
@@ -6,7 +6,6 @@ sealed interface ManageChatAction {
     data object OnPrimaryActionButton : ManageChatAction
 
     sealed interface ChatParticipantsAction : ManageChatAction {
-        data object OnAddParticipantClick : ManageChatAction
         data class OnSelectChat(val chatId: String) : ManageChatAction
     }
 }
