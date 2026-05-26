5d3862d573334c5f0348e1628d3e5978119c167e
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-26 23:31:24 +0200

    feature(di): create dependency of ConnectivityObserver in ios platform

--- feature/chat/data/src/iosMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.ios.kt
@@ -1,6 +1,7 @@
 package com.example.feature.chat.data.di
 
 import com.example.feature.chat.data.lifecycle.AppLifecycleObserver
+import com.example.feature.chat.data.network.ConnectivityObserver
 import com.example.feature.chat.database.ChirpDatabaseFactory
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
@@ -9,4 +10,5 @@ import org.koin.dsl.module
 actual val platformChatDataModule: Module = module {
     single { ChirpDatabaseFactory() }
     singleOf(::AppLifecycleObserver)
+    singleOf(::ConnectivityObserver)
 }
