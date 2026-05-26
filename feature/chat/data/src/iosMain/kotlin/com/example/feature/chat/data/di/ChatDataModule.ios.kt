97e75166fc8bff23c853a16d20ee486eba3f69b2
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-26 22:47:51 +0200

    feature(di): create dependency of AppLifecycleObserver in ios platform

--- feature/chat/data/src/iosMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.ios.kt
@@ -1,9 +1,12 @@
 package com.example.feature.chat.data.di
 
+import com.example.feature.chat.data.lifecycle.AppLifecycleObserver
 import com.example.feature.chat.database.ChirpDatabaseFactory
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val platformChatDataModule: Module = module {
     single { ChirpDatabaseFactory() }
+    singleOf(::AppLifecycleObserver)
 }
