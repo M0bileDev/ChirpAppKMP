5bac71b736f269bee6211c993f2aed058920fae3
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-28 22:02:58 +0200

    feature(network): create properties that represent connection state, connection status and foreground state

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/network/KtorWebSocketConnector.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/network/KtorWebSocketConnector.kt
@@ -1,11 +1,22 @@
+@file:OptIn(FlowPreview::class)
+
 package com.example.feature.chat.data.network
 
 import com.example.core.domain.auth.SessionStorage
 import com.example.core.domain.logging.ChirpLogger
 import com.example.feature.chat.data.lifecycle.AppLifecycleObserver
+import com.example.feature.chat.domain.model.ConnectionState
 import io.ktor.client.HttpClient
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.seconds
 
 class KtorWebSocketConnector(
     private val httpClient: HttpClient,
@@ -18,4 +29,29 @@ class KtorWebSocketConnector(
     private val connectivityObserver: ConnectivityObserver,
     private val logger: ChirpLogger
 ) {
+
+    private val _connectionState = MutableStateFlow(ConnectionState.DISCONNECTED)
+    val connectionState = _connectionState.asStateFlow()
+
+    private val isConnected = connectivityObserver
+        .isConnected
+        .debounce(1.seconds)
+        .stateIn(
+            applicationScope,
+            SharingStarted.WhileSubscribed(stopTimeoutMillis = 5_000L),
+            false
+        )
+
+    private val isInForeground = appLifecycleObserver
+        .isInForeground
+        .onEach { isInForeground ->
+            if (isInForeground) {
+                connectionRetryHandler.resetDelay()
+            }
+        }
+        .stateIn(
+            applicationScope,
+            SharingStarted.WhileSubscribed(5_000L),
+            false
+        )
 }
