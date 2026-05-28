0292ee8fbdef20e1cf30a4889fc14a39b483c660
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-28 21:36:13 +0200

    feature(network): create function that creates back delay based on attempt

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/network/ConnectionRetryHandler.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/network/ConnectionRetryHandler.kt
@@ -1,5 +1,7 @@
 package com.example.feature.chat.data.network
 
+import kotlin.math.pow
+
 class ConnectionRetryHandler(
     private val connectionErrorHandler: ConnectionErrorHandler
 ) {
@@ -7,5 +9,13 @@ class ConnectionRetryHandler(
 
     fun shouldRetry(cause: Throwable): Boolean {
         return connectionErrorHandler.isRetriableError(cause)
+    }
+
+    private fun createBackoffDelay(attempt: Int): Long {
+        // 2 to power of attempt * 2 seconds
+        val delayTime = 2f.pow(attempt).toLong() * 2_000L
+        // 30 seconds
+        val maxDelay = 30_000L
+        return minOf(delayTime, maxDelay)
     }
 }
