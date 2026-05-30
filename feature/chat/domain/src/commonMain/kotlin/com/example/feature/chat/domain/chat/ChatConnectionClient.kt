3f778d079d0d599e818405dbcd20c155947d10c5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-30 22:43:03 +0200

    feature(chat): create abstraction over chat connection client

---
+++ feature/chat/domain/src/commonMain/kotlin/com/example/feature/chat/domain/chat/ChatConnectionClient.kt
@@ -0,0 +1,14 @@
+package com.example.feature.chat.domain.chat
+
+import com.example.core.domain.util.EmptyResult
+import com.example.feature.chat.domain.error.ConnectionError
+import com.example.feature.chat.domain.model.ChatMessage
+import com.example.feature.chat.domain.model.ConnectionState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+interface ChatConnectionClient {
+    val chatMessages: Flow<ChatMessage>
+    val connectionState: StateFlow<ConnectionState>
+    suspend fun sendChatMessage(message: String): EmptyResult<ConnectionError>
+}
