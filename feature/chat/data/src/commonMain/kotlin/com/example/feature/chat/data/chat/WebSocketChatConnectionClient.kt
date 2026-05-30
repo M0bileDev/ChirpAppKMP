450bdf6959d46fa1e093c24b46c6022f00ca5635
m0bileDev <damian.ogorek.1996@gmail.com>
2026-05-30 22:46:57 +0200

    feature(chat): add properties that support implementation

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/WebSocketChatConnectionClient.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/chat/WebSocketChatConnectionClient.kt
@@ -1,14 +1,25 @@
 package com.example.feature.chat.data.chat
 
+import com.example.core.domain.auth.SessionStorage
 import com.example.core.domain.util.EmptyResult
+import com.example.feature.chat.data.network.KtorWebSocketConnector
+import com.example.feature.chat.database.ChirpChatDatabase
 import com.example.feature.chat.domain.chat.ChatConnectionClient
+import com.example.feature.chat.domain.chat.ChatRepository
 import com.example.feature.chat.domain.error.ConnectionError
 import com.example.feature.chat.domain.model.ChatMessage
 import com.example.feature.chat.domain.model.ConnectionState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.serialization.json.Json
 
-class WebSocketChatConnectionClient : ChatConnectionClient {
+class WebSocketChatConnectionClient(
+    private val ktorWebSocketConnector: KtorWebSocketConnector,
+    private val chatRepository: ChatRepository,
+    private val chirpChatDatabase: ChirpChatDatabase,
+    private val sessionStorage: SessionStorage,
+    private val json: Json
+) : ChatConnectionClient {
     override val chatMessages: Flow<ChatMessage>
         get() = TODO("Not yet implemented")
     override val connectionState: StateFlow<ConnectionState>
