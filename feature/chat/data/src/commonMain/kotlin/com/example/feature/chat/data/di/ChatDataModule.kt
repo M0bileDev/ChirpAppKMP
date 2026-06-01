e3bc940032525fa6aa8860ca9e1a6d63cb982d9b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-02 00:02:45 +0200

    feature(di): register Json dependency

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
@@ -1,17 +1,19 @@
 package com.example.feature.chat.data.di
 
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import com.example.feature.chat.data.chat.WebSocketChatConnectionClient
 import com.example.feature.chat.data.chat.KtorChatParticipantService
 import com.example.feature.chat.data.chat.KtorChatService
 import com.example.feature.chat.data.chat.OfflineFirstChatRepository
+import com.example.feature.chat.data.chat.WebSocketChatConnectionClient
 import com.example.feature.chat.data.message.OfflineFirstMessageRepository
+import com.example.feature.chat.data.network.KtorWebSocketConnector
 import com.example.feature.chat.database.ChirpDatabaseFactory
+import com.example.feature.chat.domain.chat.ChatConnectionClient
 import com.example.feature.chat.domain.chat.ChatParticipantService
 import com.example.feature.chat.domain.chat.ChatRepository
 import com.example.feature.chat.domain.chat.ChatService
-import com.example.feature.chat.domain.chat.ChatConnectionClient
 import com.example.feature.chat.domain.message.MessageRepository
+import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -24,13 +26,19 @@ val chatDataModule = module {
 
     singleOf(::KtorChatParticipantService) bind ChatParticipantService::class
     singleOf(::KtorChatService) bind ChatService::class
+    singleOf(::OfflineFirstChatRepository) bind ChatRepository::class
+    singleOf(::OfflineFirstMessageRepository) bind MessageRepository::class
+    singleOf(::WebSocketChatConnectionClient) bind ChatConnectionClient::class
+    singleOf(::KtorWebSocketConnector)
+    single {
+        Json {
+            ignoreUnknownKeys = true
+        }
+    }
     single {
         get<ChirpDatabaseFactory>()
             .create()
             .setDriver(BundledSQLiteDriver())
             .build()
     }
-    singleOf(::OfflineFirstChatRepository) bind ChatRepository::class
-    singleOf(::WebSocketChatConnectionClient) bind ChatConnectionClient::class
-    singleOf(::OfflineFirstMessageRepository) bind MessageRepository::class
 }
