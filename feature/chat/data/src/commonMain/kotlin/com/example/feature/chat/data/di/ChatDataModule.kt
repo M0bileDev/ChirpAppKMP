f7906def4c82c5def7c512ff3cb3e774e50d5868
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-02 23:44:45 +0200

    feature(di): register dependency

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.kt
@@ -5,6 +5,7 @@ import com.example.feature.chat.data.chat.KtorChatParticipantService
 import com.example.feature.chat.data.chat.KtorChatService
 import com.example.feature.chat.data.chat.OfflineFirstChatRepository
 import com.example.feature.chat.data.chat.WebSocketChatConnectionClient
+import com.example.feature.chat.data.message.KtorChatMessageService
 import com.example.feature.chat.data.message.OfflineFirstMessageRepository
 import com.example.feature.chat.data.network.KtorWebSocketConnector
 import com.example.feature.chat.database.ChirpDatabaseFactory
@@ -12,6 +13,7 @@ import com.example.feature.chat.domain.chat.ChatConnectionClient
 import com.example.feature.chat.domain.chat.ChatParticipantService
 import com.example.feature.chat.domain.chat.ChatRepository
 import com.example.feature.chat.domain.chat.ChatService
+import com.example.feature.chat.domain.message.ChatMessageService
 import com.example.feature.chat.domain.message.MessageRepository
 import kotlinx.serialization.json.Json
 import org.koin.core.module.Module
@@ -29,6 +31,7 @@ val chatDataModule = module {
     singleOf(::OfflineFirstChatRepository) bind ChatRepository::class
     singleOf(::OfflineFirstMessageRepository) bind MessageRepository::class
     singleOf(::WebSocketChatConnectionClient) bind ChatConnectionClient::class
+    singleOf(::KtorChatMessageService) bind ChatMessageService::class
     singleOf(::KtorWebSocketConnector)
     single {
         Json {
