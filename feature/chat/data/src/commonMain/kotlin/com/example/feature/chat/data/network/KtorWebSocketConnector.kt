321122c0c179d259f7ce9053943c865125225306
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-04 22:20:59 +0200

    refactor(network): change type

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/network/KtorWebSocketConnector.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/network/KtorWebSocketConnector.kt
@@ -5,12 +5,12 @@ package com.example.feature.chat.data.network
 import com.example.core.data.network.UrlConstants.BASE_URL_WS
 import com.example.core.domain.auth.SessionStorage
 import com.example.core.domain.logging.ChirpLogger
+import com.example.core.domain.util.DataError
 import com.example.core.domain.util.EmptyResult
 import com.example.core.domain.util.Result
 import com.example.feature.chat.data.BuildKonfig
 import com.example.feature.chat.data.dto.websocket.WebSocketMessageDto
 import com.example.feature.chat.data.lifecycle.AppLifecycleObserver
-import com.example.feature.chat.domain.error.ConnectionError
 import com.example.feature.chat.domain.model.ConnectionState
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.websocket.webSocketSession
@@ -215,9 +215,9 @@ class KtorWebSocketConnector(
         }
     }
 
-    suspend fun sendMessage(message: String): EmptyResult<ConnectionError> {
+    suspend fun sendMessage(message: String): EmptyResult<DataError.Connection> {
         if (webSocketSession == null || connectionState.value != ConnectionState.CONNECTED) {
-            return Result.Failure(ConnectionError.NOT_CONNECTED)
+            return Result.Failure(DataError.Connection.NOT_CONNECTED)
         }
 
         return try {
@@ -226,7 +226,7 @@ class KtorWebSocketConnector(
         } catch (e: Exception) {
             currentCoroutineContext().ensureActive()
             logger.error("Unable to send WebSocket message", e)
-            Result.Failure(ConnectionError.MESSAGE_SEND_FAILED)
+            Result.Failure(DataError.Connection.MESSAGE_SEND_FAILED)
         }
     }
 }
