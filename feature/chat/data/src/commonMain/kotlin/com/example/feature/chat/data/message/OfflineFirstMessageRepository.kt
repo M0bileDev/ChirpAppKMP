552f8ae5bdd9f33496726fbc9c16ab06f51b7359
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-04 22:32:40 +0200

    feature(message): add session storage property
    Implement sendMessage function (wip)

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/message/OfflineFirstMessageRepository.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/message/OfflineFirstMessageRepository.kt
@@ -1,12 +1,14 @@
 package com.example.feature.chat.data.message
 
 import com.example.core.data.database.safeDatabaseUpdate
+import com.example.core.domain.auth.SessionStorage
 import com.example.core.domain.util.DataError
 import com.example.core.domain.util.EmptyResult
 import com.example.core.domain.util.Result
 import com.example.core.domain.util.onSuccess
 import com.example.feature.chat.data.mappers.toDomain
 import com.example.feature.chat.data.mappers.toEntity
+import com.example.feature.chat.data.mappers.toWebsocketDto
 import com.example.feature.chat.data.message.ChatMessageConstants.PAGE_SIZE
 import com.example.feature.chat.database.ChirpChatDatabase
 import com.example.feature.chat.domain.message.ChatMessageService
@@ -14,14 +16,18 @@ import com.example.feature.chat.domain.message.MessageRepository
 import com.example.feature.chat.domain.model.ChatMessage
 import com.example.feature.chat.domain.model.ChatMessageDeliveryStatus
 import com.example.feature.chat.domain.model.MessageWithSender
+import com.example.feature.chat.domain.model.OutgoingNewMessage
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlin.time.Clock
 
 class OfflineFirstMessageRepository(
     private val chirpChatDatabase: ChirpChatDatabase,
-    private val chatMessageService: ChatMessageService
+    private val chatMessageService: ChatMessageService,
+    private val sessionStorage: SessionStorage
 ) : MessageRepository {
+
     override suspend fun updateMessageDeliveryStatus(
         messageId: String,
         status: ChatMessageDeliveryStatus
@@ -63,5 +69,25 @@ class OfflineFirstMessageRepository(
             .map { messages ->
                 messages.map { message -> message.toDomain() }
             }
+    }
+
+    override suspend fun sendMessage(message: OutgoingNewMessage): EmptyResult<DataError> {
+        return safeDatabaseUpdate {
+            val dto = message.toWebsocketDto()
+            val localUser =
+                sessionStorage.observeAuthInfo().first()?.user ?: return Result.Failure(
+                    DataError.Local.NOT_FOUND
+                )
+            val entity = dto.toEntity(
+                senderId = localUser.id,
+                deliveryStatus = ChatMessageDeliveryStatus.SENDING
+            )
+
+            chirpChatDatabase.chatMessageDao.upsertMessage(
+                message = entity
+            )
+
+            // TODO: send message through websocket
+        }
     }
 }
