07c862aa367613d392a69bc6cd01951290390877
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-06 23:51:47 +0200

    refactor(chat_detail): remove contentWindowInsets because it caused additional padding

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -130,7 +128,6 @@ fun ChatDetailScreen(
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        contentWindowInsets = WindowInsets.safeDrawing,
         containerColor = containerColor,
         snackbarHost = {
             SnackbarHost(snackbarState)
