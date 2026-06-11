d22cd9691e7ed9f075f28aca149b2baf547af3c5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-11 22:30:52 +0200

    refactor(chat_detail): rename action

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailAction.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailAction.kt
@@ -15,5 +15,5 @@ sealed interface ChatDetailAction {
     data object OnChatMembersClick : ChatDetailAction
     data object OnLeaveChatClick : ChatDetailAction
     data object OnDismissChatOptions : ChatDetailAction
-    data object OnRetryPaginationClick : ChatDetailAction
+    data object OnPaginationRetryClick : ChatDetailAction
 }
