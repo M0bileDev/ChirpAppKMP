957952aaa1c4ba32cd32535f925fa94160cbdd11
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-13 23:06:04 +0200

    feature(chat_detail): add action that represents messages scroll first index was changed

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailAction.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/chat_detail/ChatDetailAction.kt
@@ -18,4 +18,6 @@ sealed interface ChatDetailAction {
     data object OnPaginationRetryClick : ChatDetailAction
     data class OnMessagesScrollTopIndexChanged(val topVisibleIndex: Int) : ChatDetailAction
     data object OnHideMessagesBanner : ChatDetailAction
+    data class OnMessagesScrollFirstIndexChanged(val index: Int) : ChatDetailAction
+
 }
