32422232ce44a7efcb59706d8b69005b1c651de5
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-15 22:32:26 +0200

    feature(profile): create portrait configuration

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/profile/components/ProfileSectionLayout.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/profile/components/ProfileSectionLayout.kt
@@ -1,8 +1,17 @@
 package com.example.feature.chat.presentation.profile.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.core.designsystem.theme.extended
 import com.example.core.presentation.composableUtil.currentDeviceConfiguration
 import com.example.core.presentation.util.DeviceConfiguration
 
@@ -15,7 +24,28 @@ fun ProfileSectionLayout(
     val deviceConfiguration = currentDeviceConfiguration()
 
     when (deviceConfiguration) {
-        DeviceConfiguration.MOBILE_PORTRAIT -> {}
+        DeviceConfiguration.MOBILE_PORTRAIT -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                Text(
+                    text = headerText,
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.extended.textTertiary
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    content()
+                }
+            }
+        }
         else -> {}
     }
 }
