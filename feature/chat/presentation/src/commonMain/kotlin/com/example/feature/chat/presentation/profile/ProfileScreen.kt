6312ba2de977b3812f5edb2bcfe5ae4b92a288dd
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-16 21:24:25 +0200

    feature(profile): provide content that represents profile image

--- feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/profile/ProfileScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/example/feature/chat/presentation/profile/ProfileScreen.kt
@@ -3,20 +3,42 @@
 package com.example.feature.chat.presentation.profile
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import chirpappkmp.feature.chat.presentation.generated.resources.Res
+import chirpappkmp.feature.chat.presentation.generated.resources.delete
+import chirpappkmp.feature.chat.presentation.generated.resources.profile_image
+import chirpappkmp.feature.chat.presentation.generated.resources.upload_icon
+import chirpappkmp.feature.chat.presentation.generated.resources.upload_image
+import com.example.core.designsystem.components.avatar.AvatarSize
+import com.example.core.designsystem.components.avatar.ChirpAvatarPhoto
 import com.example.core.designsystem.components.brand.ChirpHorizontalDivider
+import com.example.core.designsystem.components.buttons.ChirpButton
+import com.example.core.designsystem.components.buttons.ChirpButtonStyle
 import com.example.core.designsystem.theme.ChirpTheme
+import com.example.core.presentation.util.UiText
 import com.example.core.presentation.util.clearFocusOnTap
 import com.example.feature.chat.presentation.profile.components.ProfileHeaderSection
+import com.example.feature.chat.presentation.profile.components.ProfileSectionLayout
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.viewmodel.koinViewModel
 import kotlin.uuid.ExperimentalUuidApi
@@ -45,14 +67,71 @@ fun ProfileScreen(
         ProfileHeaderSection(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(
-                    vertical = 16.dp,
-                    horizontal = 20.dp
-                ),
+                .padding(16.dp),
             username = username,
             onCloseClick = { onAction(ProfileAction.OnDismiss) }
         )
         ChirpHorizontalDivider()
+        ProfileSectionLayout(
+            headerText = stringResource(Res.string.profile_image)
+        ) {
+            Row {
+                ChirpAvatarPhoto(
+                    displayText = userInitials,
+                    size = AvatarSize.LARGE,
+                    imageUrl = profilePictureUrl,
+                    onClick = {
+                        onAction(ProfileAction.OnUploadPictureClick)
+                    }
+                )
+                Spacer(modifier = Modifier.width(20.dp))
+                FlowRow(
+                    modifier = Modifier
+                        .weight(1f),
+                    horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    ChirpButton(
+                        text = stringResource(Res.string.upload_image),
+                        onClick = {
+                            onAction(ProfileAction.OnUploadPictureClick)
+                        },
+                        style = ChirpButtonStyle.SECONDARY,
+                        enabled = !isUploadingImage && !isDeletingImage,
+                        isLoading = isUploadingImage,
+                        leadingIcon = {
+                            Icon(
+                                imageVector = vectorResource(Res.drawable.upload_icon),
+                                contentDescription = stringResource(Res.string.upload_image)
+                            )
+                        }
+                    )
+                    ChirpButton(
+                        text = stringResource(Res.string.delete),
+                        onClick = {
+                            onAction(ProfileAction.OnDeletePictureClick)
+                        },
+                        style = ChirpButtonStyle.DESTRUCTIVE_SECONDARY,
+                        enabled = !isUploadingImage && !isDeletingImage && profilePictureUrl != null,
+                        isLoading = isDeletingImage,
+                        leadingIcon = {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = stringResource(Res.string.delete)
+                            )
+                        }
+                    )
+                }
+            }
+
+            if (imageError != null) {
+                Text(
+                    text = imageError.asString(),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        }
     }
 }
 
@@ -62,7 +141,8 @@ fun PreviewProfileScreen() {
     ChirpTheme {
         ProfileScreen(
             state = ProfileState(
-                username = "Lorem ipsum"
+                username = "Lorem ipsum",
+                imageError = UiText.DynamicString("Lorem ipsum")
             ),
             onAction = {}
         )
@@ -77,7 +157,8 @@ fun PreviewDarkProfileScreen() {
     ) {
         ProfileScreen(
             state = ProfileState(
-                username = "Lorem ipsum"
+                username = "Lorem ipsum",
+                imageError = UiText.DynamicString("Lorem ipsum")
             ),
             onAction = {}
         )
