42360c538ed487c2e14566c377431cfb66b7a04b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-19 23:30:24 +0200

    feature(mediapicker): create android specific @Composable image picker

---
+++ feature/chat/presentation/src/androidMain/kotlin/com/example/feature/chat/presentation/profile/mediapicker/rememberImagePickerLauncher.android.kt
@@ -0,0 +1,48 @@
+package com.example.feature.chat.presentation.profile.mediapicker
+
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
+
+@Composable
+actual fun rememberImagePicker(
+    onResult: (PickedImageData) -> Unit
+): ImagePickerLauncher {
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val request = PickVisualMediaRequest(
+        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly
+    )
+    val photoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) { uri ->
+        if (uri != null) {
+            val parser = ContentUriParser(context)
+            val mimeType = parser.getMimeType(uri)
+
+            scope.launch {
+                val data = PickedImageData(
+                    bytes = parser.readUri(uri) ?: return@launch,
+                    mimeType = mimeType
+                )
+                onResult(data)
+            }
+        }
+    }
+
+
+    return remember {
+        ImagePickerLauncher(
+            onLaunch = {
+                photoPickerLauncher.launch(
+                    input = request
+                )
+            }
+        )
+    }
+}
