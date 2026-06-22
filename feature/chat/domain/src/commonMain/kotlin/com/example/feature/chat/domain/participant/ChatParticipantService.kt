c6da2a5b29548a5741214fcf96767906942ce738
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-22 21:13:34 +0200

    feature(participant): define abstract function that defines deleteProfilePicture call

--- feature/chat/domain/src/commonMain/kotlin/com/example/feature/chat/domain/participant/ChatParticipantService.kt
+++ feature/chat/domain/src/commonMain/kotlin/com/example/feature/chat/domain/participant/ChatParticipantService.kt
@@ -27,4 +27,6 @@ interface ChatParticipantService {
     suspend fun confirmProfilePictureUpload(
         publicUrl: String
     ): EmptyResult<DataError.Remote>
+
+    suspend fun deleteProfilePicture(): EmptyResult<DataError.Remote>
 }
