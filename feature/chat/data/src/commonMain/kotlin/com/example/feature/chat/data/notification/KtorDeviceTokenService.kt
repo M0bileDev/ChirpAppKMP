8fdd97397104725af82d23b90492c847c623e2f6
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-24 23:20:46 +0200

    feature(notification): provide implementation of registerToken function

--- feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/notification/KtorDeviceTokenService.kt
+++ feature/chat/data/src/commonMain/kotlin/com/example/feature/chat/data/notification/KtorDeviceTokenService.kt
@@ -1,19 +1,27 @@
 package com.example.feature.chat.data.notification
 
+import com.example.core.data.network.post
 import com.example.core.domain.util.DataError
 import com.example.core.domain.util.EmptyResult
+import com.example.feature.chat.data.dto.request.RegisterDeviceTokenRequest
 import com.example.feature.chat.domain.notification.DeviceTokenService
 import io.ktor.client.HttpClient
 
 class KtorDeviceTokenService(
     private val httpClient: HttpClient
-) : DeviceTokenService{
+) : DeviceTokenService {
 
     override suspend fun registerToken(
         token: String,
         platform: String
     ): EmptyResult<DataError.Remote> {
-        TODO("Not yet implemented")
+        return httpClient.post(
+            route = "/notification/register",
+            body = RegisterDeviceTokenRequest(
+                token = token,
+                platform = platform
+            )
+        )
     }
 
     override suspend fun unregisterToken(token: String): EmptyResult<DataError.Remote> {
