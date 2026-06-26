2e499c3e777df2f1c9d53bb282f8a3a0afa89412
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-26 20:37:15 +0200

    feature(di): register FirebasePushNotificationService in di

--- feature/chat/data/src/iosMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.ios.kt
+++ feature/chat/data/src/iosMain/kotlin/com/example/feature/chat/data/di/ChatDataModule.ios.kt
@@ -3,9 +3,12 @@ package com.example.feature.chat.data.di
 import com.example.feature.chat.data.lifecycle.AppLifecycleObserver
 import com.example.feature.chat.data.network.ConnectionErrorHandler
 import com.example.feature.chat.data.network.ConnectivityObserver
+import com.example.feature.chat.data.notification.FirebasePushNotificationService
 import com.example.feature.chat.database.ChirpDatabaseFactory
+import com.example.feature.chat.domain.notification.PushNotificationTokenService
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val platformChatDataModule: Module = module {
@@ -13,4 +16,5 @@ actual val platformChatDataModule: Module = module {
     singleOf(::AppLifecycleObserver)
     singleOf(::ConnectivityObserver)
     singleOf(::ConnectionErrorHandler)
+    singleOf(::FirebasePushNotificationService) bind PushNotificationTokenService::class
 }
