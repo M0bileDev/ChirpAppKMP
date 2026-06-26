371c4766a6b1f6da343e84d5115abfbcb70450a7
m0bileDev <damian.ogorek.1996@gmail.com>
2026-06-26 20:36:35 +0200

    refactor(presentation): remove iosX64 target

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -38,7 +38,6 @@ kotlin {
         }
         listOf(
             iosArm64(),
-            iosX64(),
             iosSimulatorArm64()
         ).forEach { target ->
             getByName("${target.name}Main") {
